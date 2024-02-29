0b7fb3f4a3379a56d4828254936bbdbb368fd35b
Zai Shi <zaishi00@outlook.com>
2024-02-29 10:48:52 +0100

    Update site title and tagline for docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Stack Docs",
+  tagline: "Developer Friendly Next.js Authentication",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -65,10 +65,11 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        // title: "My Site",
+        // title: "Stack",
         logo: {
           alt: "Stack",
           src: "img/logo.svg",
+          href: "/docs/setup",
         },
         items: [
           {
