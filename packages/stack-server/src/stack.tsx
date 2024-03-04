5689e4125f389ca0283124bed3cf63f03002b012
Stan Wohlwend <n2d4xc@gmail.com>
2024-03-04 22:03:19 +0100

    Don't require NEXT_PUBLIC_STACK_URL

--- packages/stack-server/src/stack.tsx
+++ packages/stack-server/src/stack.tsx
@@ -1,8 +1,6 @@
 import { StackServerApp } from '@stackframe/stack';
-import { throwErr } from '@stackframe/stack-shared/dist/utils/errors';
 
 export const stackServerApp = new StackServerApp({
-  baseUrl: process.env.NEXT_PUBLIC_STACK_URL || throwErr('missing NEXT_PUBLIC_STACK_URL environment variable'),
   tokenStore: "nextjs-cookie",
   urls: {
     userHome: "/projects",
