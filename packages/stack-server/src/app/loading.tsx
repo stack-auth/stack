8fa3d5d00e7d3c0ca3c3df80f609dc05b52a7a33
Stan Wohlwend <n2d4xc@gmail.com>
2024-03-04 23:03:57 +0100

    Loading indicator

--- packages/stack-server/src/app/loading.tsx
+++ packages/stack-server/src/app/loading.tsx
@@ -1,9 +1,10 @@
+import { PageLoadingIndicator } from "@/components/page-loading-indicator";
 import { Paragraph } from "@/components/paragraph";
 
 export default function Loading() {
   return (
     <Paragraph body>
-      Loading...
+      <PageLoadingIndicator />
     </Paragraph>
   );
 }
