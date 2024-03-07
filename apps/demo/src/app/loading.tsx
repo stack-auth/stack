08369135aafd1af4c2cc8333805f0f9645f58d23
Zai Shi <zaishi00@outlook.com>
2024-03-07 23:33:38 +0800

    upgraded next version in demo, added loading indicator

--- apps/demo/src/app/loading.tsx
+++ apps/demo/src/app/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading() {
-  return <>
-    Loading...
-  </>;
+  return <div className="flex items-center justify-center h-screen">
+    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+  </div>;
 }
