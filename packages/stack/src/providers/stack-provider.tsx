1125ffbe7fc06d53103e649c8397a9f0812eacda
Zai Shi <zaishi00@outlook.com>
2024-03-16 11:00:02 +0800

    updated package config

--- packages/stack/src/providers/stack-provider.tsx
+++ packages/stack/src/providers/stack-provider.tsx
@@ -1,18 +1,23 @@
-import { ReactNode } from 'react';
+import React from 'react';
 import { StackClientApp, stackAppInternalsSymbol } from '../lib/stack-app';
 import { StackProviderClient } from './stack-providerClient';
+import { StackUIProvider, ThemeConfig } from '@stackframe/stack-ui';
 
 
 export default function StackProvider({
   children,
   app,
+  theme,
 }: {
-  children: ReactNode,
+  children: React.ReactNode,
   app: StackClientApp<true>,
+  theme?: ThemeConfig,
 }) {
   return (
     <StackProviderClient appJsonPromise={app[stackAppInternalsSymbol].toClientJson()}>
-      {children}
+      <StackUIProvider theme={theme}>
+        {children}
+      </StackUIProvider>
     </StackProviderClient>
   );
 }
