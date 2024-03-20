0e5d9e365dcb931cbdef26fa08674fc08e63f161
Zai Shi <zaishi00@outlook.com>
2024-03-20 18:09:09 +0800

    cleaned up package.json files

--- packages/stack-server/src/app/(main)/layout.tsx
+++ packages/stack-server/src/app/(main)/layout.tsx
@@ -1,13 +1,13 @@
 import { StackProvider } from "@stackframe/stack";
 import { stackServerApp } from "@/stack";
-import { StackUIJoyProvider } from "@stackframe/stack-ui-joy";
+// import { StackUIJoyProvider } from "@stackframe/stack-ui-joy";
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
     <StackProvider app={stackServerApp}>
-      <StackUIJoyProvider>
-        {props.children}
-      </StackUIJoyProvider>
+      {/* <StackUIJoyProvider> */}
+      {props.children}
+      {/* </StackUIJoyProvider> */}
     </StackProvider>
   );
 }
