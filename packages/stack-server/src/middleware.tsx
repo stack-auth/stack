80238c5e3e35a7991a9e407154f63b803a6f8fc9
Stan Wohlwend <n2d4xc@gmail.com>
2024-04-11 15:31:29 +0200

    Fix CORS headers

--- packages/stack-server/src/middleware.tsx
+++ packages/stack-server/src/middleware.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { wait } from '@stackframe/stack-shared/dist/utils/promises';
 
-const allowedCorsHeaders = [
+const corsAllowedRequestHeaders = [
   'authorization',
   'content-type',
   'x-stack-project-id',
@@ -10,21 +10,40 @@ const allowedCorsHeaders = [
   'x-stack-secret-server-key',
   'x-stack-super-secret-admin-key',
   'x-stack-admin-access-token',
+  'x-stack-override-error-status',
+];
+
+const corsAllowedResponseHeaders = [
+  'content-type',
+  'x-stack-actual-status',
+  'x-stack-known-error',
 ];
  
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  if (request.method === 'OPTIONS') {
-    return new Response(null, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': allowedCorsHeaders.join(', '),
+  const url = new URL(request.url);
+  const isApiRequest = url.pathname.startsWith('/api/');
+
+  // default headers
+  const responseInit: ResponseInit = {
+    headers: {
+      // CORS headers
+      ...!isApiRequest ? {} : {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": corsAllowedRequestHeaders.join(', '),
+        "Access-Control-Expose-Headers": corsAllowedResponseHeaders.join(', '),
       },
-    });
+    },
+  };
+
+  // we want to allow preflight requests to pass through
+  // even if the API route does not implement OPTIONS
+  if (request.method === 'OPTIONS' && isApiRequest) {
+    return new Response(null, responseInit);
   }
 
-  return NextResponse.next();
+  return NextResponse.next(responseInit);
 }
  
 // See "Matching Paths" below to learn more
