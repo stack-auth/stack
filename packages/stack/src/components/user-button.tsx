41ae00f70543578d7e15178f5ebbc4f1e50f2a65
Zai Shi <zaishi00@outlook.com>
2024-04-11 17:37:36 +0200

    improved user button

--- packages/stack/src/components/user-button.tsx
+++ packages/stack/src/components/user-button.tsx
@@ -1,49 +1,27 @@
 'use client';
 import React from "react";
-import { Popover, PopoverTrigger, PopoverContent, useUser, Text, Divider, useDesign, Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator } from "..";
+import { useUser, Text, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator } from "..";
 import { RxPerson, RxEnter } from "react-icons/rx";
 import { useRouter } from "next/navigation";
+import { runAsynchronously } from "@stackframe/stack-shared/dist/utils/promises";
 
-function Item({ text, icon, href }: { text: string, icon: React.ReactNode, href: string }) {
+function Item(props: { text: string, icon: React.ReactNode, href?: string, onClick?: () => void }) {
   const router = useRouter();
   return (
-    <DropdownMenuItem onClick={() => router.push(href)} style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
-      {icon}
-      <Text>{text}</Text>
+    <DropdownMenuItem 
+      onClick={props.onClick || (() => router.push(props.href || ''))}
+      style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}
+    >
+      {props.icon}
+      <Text>{props.text}</Text>
     </DropdownMenuItem>
   );
 }
-
 
 export default function UserButton() {
   const user = useUser();
   if (!user) return null;
 
-  // return (
-  //   <Popover>
-  //     <PopoverTrigger>
-  //       <div style={{ display: 'flex', alignItems: 'center', height: 40, padding: '0.25rem' }}>
-  //         <img src={user.profileImageUrl || ''} style={{ borderRadius: 100, height: '100%' }} />
-  //       </div>
-  //     </PopoverTrigger>
-  //     <PopoverContent>
-  //       <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-  //         <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
-  //           <img src={user.profileImageUrl || ''} style={{ borderRadius: 100, height: 40, width: 40 }} />
-  //           <div>
-  //             <Text>{user.displayName}</Text>
-  //             <Text variant="secondary" size="sm">{user.primaryEmail}</Text>
-  //           </div>
-  //         </div>
-  //         <Divider />
-  //         <div style={{ display: 'flex', flexDirection: 'column'}}>
-  //           <PopoverItem text="Account settings" href="/profile" icon={<RxPerson size={22} color={'grey'}/>} />
-  //           <PopoverItem text="Sign Out" href="/logout" icon={<RxEnter size={22} color={'grey'}/>} />
-  //         </div>
-  //       </div>
-  //     </PopoverContent>
-  //   </Popover>
-  // );
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -63,7 +41,7 @@ export default function UserButton() {
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <Item text="Account settings" href="/profile" icon={<RxPerson size={22} color={'grey'} />} />
-        <Item text="Sign Out" href="/logout" icon={<RxEnter size={22} color={'grey'} />} />
+        <Item text="Sign Out" onClick={() => runAsynchronously(user.signOut())} icon={<RxEnter size={22} color={'grey'} />} />
       </DropdownMenuContent>
     </DropdownMenu>
   );
