c798438f4db677a7690930812c5106e478800fbb
Zai Shi <zaishi00@outlook.com>
2024-04-19 13:53:41 +0200

    restructured sign in sign up page

--- packages/stack/src/components-page/sign-in.tsx
+++ packages/stack/src/components-page/sign-in.tsx
@@ -1,57 +1,6 @@
 'use client';
-
-import CredentialSignIn from '../components/credential-sign-in';
-import SeparatorWithText from '../components/separator-with-text';
-import OAuthGroup from '../components/oauth-group';
-import CardFrame from '../components/card-frame';
-import { useUser, useStackApp } from '..';
-import RedirectMessageCard from '../components/redirect-message-card';
-import { Link, Tabs, TabsContent, TabsList, TabsTrigger, Text } from "../components-core";
-import MagicLinkSignIn from '../components/magic-link-sign-in';
+import AuthPage from './auth-page';
 
 export default function SignIn({ fullPage=false }: { fullPage?: boolean }) {
-  const stackApp = useStackApp();
-  const user = useUser();
-  const project = stackApp.useProject();
-
-  if (user) {
-    return <RedirectMessageCard type='signedIn' fullPage={fullPage} />;
-  }
-
-  const enableSeparator = project.credentialEnabled && project.oauthProviders.filter(p => p.enabled).length > 0;
-
-  return (
-    <CardFrame fullPage={fullPage}>
-      <div style={{ textAlign: 'center', marginBottom: '1.5rem' }}>
-        <Text size="xl" as='h2'>Sign into Your Account</Text>
-        <Text>
-          {"Don't have an account? "}
-          <Link href={stackApp.urls['signUp']}>
-            Sign up
-          </Link>
-        </Text>
-      </div>
-      <OAuthGroup type='signin'/>
-      {enableSeparator && <SeparatorWithText text={'Or continue with'} />}
-
-      {project.credentialEnabled && project.magicLinkEnabled ? (
-        <Tabs defaultValue='magic-link'>
-          <TabsList style={{ marginBottom: '1.5rem' }}>
-            <TabsTrigger value='magic-link'>Magic Link</TabsTrigger>
-            <TabsTrigger value='password'>Password</TabsTrigger>
-          </TabsList>
-          <TabsContent value='magic-link'>
-            <MagicLinkSignIn/>
-          </TabsContent>
-          <TabsContent value='password'>
-            <CredentialSignIn/>
-          </TabsContent>
-        </Tabs>
-      ) : project.credentialEnabled ? (
-        <CredentialSignIn/>
-      ) : project.magicLinkEnabled ? (
-        <MagicLinkSignIn/>
-      ) : null}
-    </CardFrame>
-  );
+  return <AuthPage fullPage={fullPage} type='sign-in' />;
 }
--- packages/stack/src/components-page/sign-up.tsx
+++ packages/stack/src/components-page/sign-up.tsx
@@ -1,39 +1,6 @@
 'use client';
-
-import SeparatorWithText from '../components/separator-with-text';
-import OAuthGroup from '../components/oauth-group';
-import CardFrame from '../components/card-frame';
-import CredentialSignUp from '../components/credential-sign-up';
-import { useUser, useStackApp } from '..';
-import RedirectMessageCard from '../components/redirect-message-card';
-import { Link, Text } from "../components-core";
+import AuthPage from './auth-page';
 
 export default function SignUp({ fullPage=false }: { fullPage?: boolean }) {
-  const stackApp = useStackApp();
-  const user = useUser();
-  const project = stackApp.useProject();
-  
-  if (user) {
-    return <RedirectMessageCard type='signedIn' fullPage={fullPage} />;
-  }
-
-  const enableSeparator = project.credentialEnabled && project.oauthProviders.filter(p => p.enabled).length > 0;
-
-  return (
-    <CardFrame fullPage={fullPage}>
-      <div style={{ textAlign: 'center', marginBottom: '1.5rem' }}>
-        <Text size="xl" as='h2'>Create a New Account</Text>
-        <Text>
-          {"Already have an account? "}
-          <Link href={stackApp.urls['signIn']}>
-            Sign in
-          </Link>
-        </Text>
-      </div>
-
-      <OAuthGroup type='signup'/>
-      {enableSeparator && <SeparatorWithText text={'or continue with email'} />}
-      {project.credentialEnabled && <CredentialSignUp/>}
-    </CardFrame>
-  );
+  return <AuthPage fullPage={fullPage} type='sign-up' />;
 }
