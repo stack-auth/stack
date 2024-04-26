38365de97134f2e6e03e3e54c5c45366b72cb1f9
Stan Wohlwend <n2d4xc@gmail.com>
2024-04-26 09:58:47 +0200

    Fix build error

--- packages/stack-server/src/components/confetti.tsx
+++ packages/stack-server/src/components/confetti.tsx
@@ -2,11 +2,12 @@
 
 import * as confetti from "canvas-confetti";
 import { useEffect } from "react";
+import { runAsynchronously } from "@stackframe/stack-shared/dist/utils/promises";
 
 
 export function Confetti() {
   useEffect(() => {
-    confetti.default()?.catch((e) => console.error(e));
+    runAsynchronously(confetti.default()?.catch((e) => console.error(e)));
   }, []);
 
   return (<></>);
