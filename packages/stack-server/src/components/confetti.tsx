27afc6567036f9c3ad3279408b5b1440b6893055
Stan Wohlwend <n2d4xc@gmail.com>
2024-04-26 09:59:51 +0200

    Confetti promise

--- packages/stack-server/src/components/confetti.tsx
+++ packages/stack-server/src/components/confetti.tsx
@@ -3,11 +3,12 @@
 import * as confetti from "canvas-confetti";
 import { useEffect } from "react";
 import { runAsynchronously } from "@stackframe/stack-shared/dist/utils/promises";
+import { throwErr } from "@stackframe/stack-shared/dist/utils/errors";
 
 
 export function Confetti() {
   useEffect(() => {
-    runAsynchronously(confetti.default()?.catch((e) => console.error(e)));
+    runAsynchronously(confetti.default() ?? throwErr("Confetti failed to load"));
   }, []);
 
   return (<></>);
