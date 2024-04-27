0fa517a000a49be4a3a85e3595fa4134d6fe013d
Stan Wohlwend <n2d4xc@gmail.com>
2024-04-27 14:30:04 +0200

    Update ESLint for partial prerendering demo

--- apps/partial-prerendering/src/app/layout.tsx
+++ apps/partial-prerendering/src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode,
 }>) {
   return (
     <html lang="en">
