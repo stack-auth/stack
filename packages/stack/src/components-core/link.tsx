1c98d78d18880d75d4fa41eee40a229c81d8e36e
Zai Shi <zaishi00@outlook.com>
2024-04-29 15:48:07 +0200

    fixed link

--- packages/stack/src/components-core/link.tsx
+++ packages/stack/src/components-core/link.tsx
@@ -3,36 +3,41 @@
 import NextLink from "next/link";
 import { FONT_FAMILY, FONT_SIZES, LINE_HEIGHTS, LINK_COLORS } from "../utils/constants";
 import { Url } from "url";
-import { useDesign } from "..";
 import React from "react";
+import styled from "styled-components";
 
 type LinkProps = {
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
   href: Url | string,
 } & Omit<React.HTMLProps<HTMLLinkElement>, 'size' | 'href'>
 
-export function Link({
-  size='md',
-  href,
-  style,
-  ...props
-} : LinkProps) {
-  const { colorMode } = useDesign();
+const StyledNextLink = styled(NextLink)<{ 
+  $size: 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+}>`
+  font-size: ${props => FONT_SIZES[props.$size]};
+  line-height: ${props => LINE_HEIGHTS[props.$size]};
+  font-family: ${FONT_FAMILY};
+  text-decoration: underline;
+  margin: 0;
+  padding: 0;
+
+  color: ${LINK_COLORS.light};
+
+  html[data-theme='dark'] & {
+    color: ${LINK_COLORS.dark};
+  }
+`;
+
+export const Link = React.forwardRef<
+  React.ElementRef<typeof StyledNextLink>,
+  LinkProps
+>(({ size='md', href, ...props }, ref) => {
   return (
-    <NextLink
+    <StyledNextLink
+      $size={size}
       href={href}
-      style={{
-        fontSize: FONT_SIZES[size],
-        lineHeight: LINE_HEIGHTS[size],
-        fontFamily: FONT_FAMILY,
-        color: LINK_COLORS[colorMode],
-        textDecoration: 'underline',
-        margin: 0,
-        padding: 0,
-        ...style
-      }}
-    >
-      {props.children}
-    </NextLink>
+      style={props.style}
+      children={props.children}
+    />
   );
-}
+});
