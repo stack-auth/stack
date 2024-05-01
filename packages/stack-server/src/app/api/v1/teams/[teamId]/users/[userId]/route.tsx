95cf608955ed8c45a29ec54abda02cfe173edcac
Zai Shi <zaishi00@outlook.com>
2024-05-01 15:17:19 +0200

    moved add-user

--- packages/stack-server/src/app/api/v1/teams/[teamId]/users/[userId]/route.tsx
+++ packages/stack-server/src/app/api/v1/teams/[teamId]/users/[userId]/route.tsx
@@ -2,8 +2,10 @@ import { NextRequest, NextResponse } from "next/server";
 import * as yup from "yup";
 import { StatusError } from "@stackframe/stack-shared/dist/utils/errors";
 import { deprecatedParseRequest, deprecatedSmartRouteHandler } from "@/lib/route-handlers";
-import { checkApiKeySet, publishableClientKeyHeaderSchema } from "@/lib/api-keys";
+import { checkApiKeySet, publishableClientKeyHeaderSchema, secretServerKeyHeaderSchema } from "@/lib/api-keys";
 import { decodeAccessToken, authorizationHeaderSchema } from "@/lib/tokens";
+import { addUserToTeam, getTeam, getUserTeams } from "@/lib/teams";
+import { getClientUser } from "@/lib/users";
 
 const getSchema = yup.object({
   method: yup.string().oneOf(["GET", "PUT"]).required(),
@@ -53,6 +55,57 @@ const handler = deprecatedSmartRouteHandler(async (req: NextRequest, options: { 
 
 export const GET = handler;
 export const PUT = handler;
+
+const postSchema = yup.object({
+  query: yup.object({
+    server: yup.string().oneOf(["true", "false"]).default("false"),
+  }).required(),
+  headers: yup.object({
+    authorization: authorizationHeaderSchema.required(),
+    "x-stack-secret-server-key": secretServerKeyHeaderSchema.default(""),
+    "x-stack-project-id": yup.string().required(),
+  }),
+  body: yup.object({
+    userId: yup.string().required(),
+  }),
+});
+
+export const POST = deprecatedSmartRouteHandler(async (req: NextRequest, options: { params: { teamId: string, userId: string } }) => {
+  const {
+    headers: {
+      "x-stack-project-id": projectId,
+      "x-stack-secret-server-key": secretServerKey,
+    },
+    query: { server },
+  } = await deprecatedParseRequest(req, postSchema);
+
+  if (!server) {
+    throw new StatusError(StatusError.BadRequest, "Not impelemented");
+  }
+
+  if (!(await checkApiKeySet(projectId, { secretServerKey }))) {
+    throw new StatusError(StatusError.Forbidden);
+  }
+
+  const team = await getTeam(projectId, options.params.teamId);
+  if (!team) {
+    throw new StatusError(StatusError.NotFound, "Team not found");
+  }
+
+  const user = await getClientUser(projectId, options.params.userId);
+  if (!user) {
+    throw new StatusError(StatusError.NotFound, "User not found");
+  }
+
+  const userTeams = await getUserTeams(projectId, options.params.userId);
+  if (userTeams.some(t => t.id === options.params.teamId)) {
+    throw new StatusError(StatusError.BadRequest, "User is already in the team");
+  }
+
+  await addUserToTeam(projectId, options.params.teamId, options.params.userId);
+
+  return NextResponse.json({});
+});
 
 
 const deleteSchema = yup.object({
