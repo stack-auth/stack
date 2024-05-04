bcce00ff599167a8c611b3eca597eb9eb7fea445
Stan Wohlwend <n2d4xc@gmail.com>
2024-05-04 15:04:54 +0200

    Disable Sentry in dev mode (#26)

--- packages/stack-server/sentry.edge.config.ts
+++ packages/stack-server/sentry.edge.config.ts
@@ -12,5 +12,7 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: process.env.NODE_ENV === "development",
+
+  enabled: process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "test",
 });
