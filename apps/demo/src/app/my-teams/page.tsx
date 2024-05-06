259deb7bfe363fb9e4088aa834755275e7305e9b
Zai Shi <zaishi00@outlook.com>
2024-05-06 16:15:01 +0200

    added list teams

---
+++ apps/demo/src/app/my-teams/page.tsx
@@ -0,0 +1,5 @@
+import PageClient from "./page-client";
+
+export default function Page() {
+  return <PageClient />;
+}
--- apps/demo/src/app/teams/page.tsx
+++ apps/demo/src/app/teams/page.tsx
@@ -1,5 +1,22 @@
-import PageClient from "./page-client";
+import { Link, Text } from "@stackframe/stack";
+import { stackServerApp } from "src/stack";
 
-export default function Page() {
-  return <PageClient />;
+export default async function Page() {
+  const teams = await stackServerApp.listTeams();
+  const user = await stackServerApp.getUser({ or: 'redirect' });
+  const userTeams = await user?.listTeams();
+
+  return (
+    <div className='flex-col gap-2'>
+      <Text size='xl'>All Teams</Text>
+
+      {teams.map((team) => (
+        <div key={team.id} className="flex gap-2 items-center">
+          <Text>{team.displayName}</Text>
+          <Text variant="secondary">{userTeams?.some(t => t.id === team.id) ? '(You are a member)' : ''}</Text>
+          <Link href={`/teams/${team.id}`}>Open</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
