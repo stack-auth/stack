0fc1ad029941d252562ccff0ab399ed295f66a72
Zai Shi <zaishi00@outlook.com>
2024-05-10 22:37:21 +0200

    added redirect to create project page

--- packages/stack-server/src/app/(main)/(protected)/(outside-dashbaord)/projects/page.tsx
+++ packages/stack-server/src/app/(main)/(protected)/(outside-dashbaord)/projects/page.tsx
@@ -1,7 +1,18 @@
 import PageClient from "./page-client";
 import Footer from "./footer";
+import { stackServerApp } from "@/stack";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const user = await stackServerApp.getUser();
+  if (!user) {
+    redirect(stackServerApp.urls.signIn);
+  }
+  const projects = await user.listOwnedProjects();
+  if (projects.length === 0) {
+    redirect("/new-project");
+  }
+  
   return (
     <>
       <div className="flex-grow p-4">
