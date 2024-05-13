3750ade9967cd90cc814ea507ab43c10cddb6dcc
Zai Shi <zaishi00@outlook.com>
2024-05-13 21:07:46 +0200

    fixed command select bug

--- packages/stack-server/src/app/test/page.tsx
+++ packages/stack-server/src/app/test/page.tsx
@@ -1,13 +1,10 @@
-import { promises as fs } from "fs";
-import path from "path";
 import { Metadata } from "next";
-import Image from "next/image";
 import { z } from "zod";
-
 import { columns } from "@/components/data-table/columns";
 import { DataTable } from "@/components/data-table/data-table";
 import { taskSchema } from "@/components/data-table/data/schema";
 import data from "@/components/data-table/data/tasks.json";
+
 
 export const metadata: Metadata = {
   title: "Tasks",
--- packages/stack-server/src/components/data-table/data-table-faceted-filter.tsx
+++ packages/stack-server/src/components/data-table/data-table-faceted-filter.tsx
@@ -42,7 +42,7 @@ export function DataTableFacetedFilter<TData, TValue>({
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button variant="outline" size="sm" className="h-8 border-dashed">
+        <Button variant="outline" size="sm" className="h-8 border">
           <PlusCircledIcon className="mr-2 h-4 w-4" />
           {title}
           {selectedValues?.size > 0 && (
