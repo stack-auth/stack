d486e1e9795aa5b594ec12364cd1f14bb63fb366
Zai Shi <zaishi00@outlook.com>
2024-05-13 18:07:04 +0200

    added data table

---
+++ packages/stack-server/src/components/data-table/config.ts
@@ -0,0 +1,50 @@
+import { MixIcon, SquareIcon } from "@radix-ui/react-icons";
+
+export type DataTableConfig = typeof dataTableConfig
+
+export const dataTableConfig = {
+  comparisonOperators: [
+    { label: "Contains", value: "ilike" as const },
+    { label: "Does not contain", value: "notIlike" as const },
+    { label: "Is", value: "eq" as const },
+    { label: "Is not", value: "notEq" as const },
+    { label: "Starts with", value: "startsWith" as const },
+    { label: "Ends with", value: "endsWith" as const },
+    { label: "Is empty", value: "isNull" as const },
+    { label: "Is not empty", value: "isNotNull" as const },
+  ],
+  selectableOperators: [
+    { label: "Is", value: "eq" as const },
+    { label: "Is not", value: "notEq" as const },
+    { label: "Is empty", value: "isNull" as const },
+    { label: "Is not empty", value: "isNotNull" as const },
+  ],
+  logicalOperators: [
+    {
+      label: "And",
+      value: "and" as const,
+      description: "All conditions must be met",
+    },
+    {
+      label: "Or",
+      value: "or" as const,
+      description: "At least one condition must be met",
+    },
+  ],
+  featureFlags: [
+    {
+      label: "Advanced filter",
+      value: "advancedFilter" as const,
+      icon: MixIcon,
+      tooltipTitle: "Toggle advanced filter",
+      tooltipDescription: "A notion like query builder to filter rows.",
+    },
+    {
+      label: "Floating bar",
+      value: "floatingBar" as const,
+      icon: SquareIcon,
+      tooltipTitle: "Toggle floating bar",
+      tooltipDescription: "A floating bar that sticks to the top of the table.",
+    },
+  ],
+};
