45a40dfaf4bca8ca7287227b76e2407f9ab409ab
Zai Shi <zaishi00@outlook.com>
2024-05-15 16:48:15 +0200

    migrated congrats page

---
+++ packages/stack-server/src/app/(main)/wizard-congrats/actions.tsx
@@ -0,0 +1,18 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+
+export default function Actions() {
+  const router = useRouter();
+  return (
+    <div className="flex gap-2 justify-center">
+      <Button variant="secondary" onClick={() => { window.open("https://docs.stack-auth.com/"); }}>
+      Visit docs
+      </Button>
+      <Button onClick={() => { router.push("/projects"); }}>
+      Continue
+      </Button>
+    </div>
+  );
+}
