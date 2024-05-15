f8338db182e4e8ef7404885e12825faada8ad5a8
Zai Shi <zaishi00@outlook.com>
2024-05-15 16:57:03 +0200

    removed unused, migrated logo

--- packages/stack-server/src/components/logo.tsx
+++ packages/stack-server/src/components/logo.tsx
@@ -1,5 +1,5 @@
-import { Link } from "@mui/joy";
 import { SmartImage } from "./smart-image";
+import Link from "next/link";
 import logoLightMode from "../../public/logo.svg";
 import logoFullLightMode from "../../public/logo-full.svg";
 import logoDarkMode from "../../public/logo-bright.svg";
@@ -9,35 +9,19 @@ type ImageProps = React.ComponentProps<typeof SmartImage>;
 
 export function Logo(props: (ImageProps | Omit<ImageProps, "src" | "alt">) & { full?: boolean, href?: string}) {
   const { full, ...imageProps } = props;
-
-  // first item is the selector which HIDES the logo, second is the logo source
-  const modes = [
-    // instead of using theme.getColorSchemeSelector, we use this literal to support RSC
-    ['html[data-joy-color-scheme="light"] &', full ? logoFullDarkMode : logoDarkMode],
-    ['html:not([data-joy-color-scheme="light"]) &', full ? logoFullLightMode : logoLightMode],
-  ] as const;
+  const logos = {
+    light: props.full ? logoFullLightMode : logoLightMode,
+    dark: props.full ? logoFullDarkMode : logoDarkMode,
+  };
 
   return (
     <>
-      {modes.map((mode, i) => (
-        <Link
-          href={props.href || "/"}
-          key={i}
-          sx={{
-            [mode[0]]: {
-              display: 'none',
-            },
-          }}
-        >
-          <SmartImage
-            src={mode[1]}
-            alt="Logo"
-            priority
-            {...imageProps}
-            placeholder="empty"
-          />
-        </Link>
-      ))}
+      <Link href={props.href || "/"} className="block dark:hidden">
+        <SmartImage src={logos.light} alt="Logo" priority {...imageProps} placeholder="empty" />
+      </Link>
+      <Link href={props.href || "/"} className="hidden dark:block">
+        <SmartImage src={logos.dark} alt="Logo" priority {...imageProps} placeholder="empty" />
+      </Link>
     </>
   );
 }
