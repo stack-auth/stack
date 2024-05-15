ad275d641f6ac5e8749416959e2bcb0d69d3f30c
Zai Shi <zaishi00@outlook.com>
2024-05-15 16:20:59 +0200

    fix navbar not sticky

--- packages/stack-server/src/components/navbar.tsx
+++ packages/stack-server/src/components/navbar.tsx
@@ -2,7 +2,6 @@
 
 import { UserButton } from "@stackframe/stack";
 import { Logo } from "./logo";
-import { Separator } from "./ui/separator";
 import Link from "next/link";
 import { useColorScheme } from "@mui/joy";
 import Typography from "./ui/typography";
@@ -10,27 +9,24 @@ import Typography from "./ui/typography";
 export function Navbar({ ...props }) {
   const { mode, setMode } = useColorScheme();
   return (
-    <div>
-      <header
-        className={`sticky top-0 z-30 flex items-center justify-between border-b-0 border-x-0 px-4 ${props.className || ""}`}
-        style={{ height: `50px` }}
-      >
-        <div className="flex items-center justify-center">
-          <Logo full height={24} href="/projects" className="h-6" />
+    <header
+      className={`sticky top-0 z-30 flex items-center justify-between border-b bg-white dark:bg-black px-4 ${props.className || ""}`}
+      style={{ height: `50px` }}
+    >
+      <div className="flex items-center justify-center">
+        <Logo full height={24} href="/projects" className="h-6" />
+      </div>
+      <div className="flex items-center">
+        <div className="flex gap-4 mr-8 items-center">
+          <Link href="mailto:team@stack-auth.com">
+            <Typography type='label'>Feedback</Typography>
+          </Link>
+          <Link href="https://docs.stack-auth.com/">
+            <Typography type='label'>Docs</Typography>
+          </Link>
         </div>
-        <div className="flex items-center">
-          <div className="flex gap-4 mr-8 items-center">
-            <Link href="mailto:team@stack-auth.com">
-              <Typography type='label'>Feedback</Typography>
-            </Link>
-            <Link href="https://docs.stack-auth.com/">
-              <Typography type='label'>Docs</Typography>
-            </Link>
-          </div>
-          <UserButton colorModeToggle={() => setMode(mode === 'light' ? 'dark' : 'light')}/>
-        </div>
-      </header>
-      <Separator/>
-    </div>
+        <UserButton colorModeToggle={() => setMode(mode === 'light' ? 'dark' : 'light')}/>
+      </div>
+    </header>
   );
 }
