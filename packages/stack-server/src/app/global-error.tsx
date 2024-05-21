adcdd8ca2c30456da092e7bb6f6213714cdf077f
Stan Wohlwend <n2d4xc@gmail.com>
2024-05-21 10:09:07 +0200

    Global error page redirect

--- packages/stack-server/src/app/global-error.tsx
+++ packages/stack-server/src/app/global-error.tsx
@@ -1,20 +1,37 @@
 "use client";
 
+import { Spinner } from "@/components/ui/spinner";
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }: any) {
+  const router = useRouter();
+  const isProdLike = process.env.NODE_ENV.includes("production");
+  
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      if (isProdLike) {
+        router.push("/");
+      }
+    }, 3000);
+  }, [router, isProdLike]);
+
   return (
     <html>
       <body suppressHydrationWarning>
-        <Error
-          statusCode={500}
-        />
+        {isProdLike ? (
+          <Spinner />
+        ) : (
+          <Error
+            statusCode={500}
+          />
+        )}
       </body>
     </html>
   );
