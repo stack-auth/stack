018f0f2e187942996110c422863056dd388f20ae
Zai Shi <zaishi00@outlook.com>
2024-05-28 11:21:46 +0200

    fixed handle bar import path

--- packages/stack-server/next.config.mjs
+++ packages/stack-server/next.config.mjs
@@ -1,4 +1,4 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import remarkGfm from "remark-gfm";
@@ -19,45 +19,47 @@ const withBundleAnalyzer = createBundleAnalyzer({
   enabled: !!process.env.ANALYZE_BUNDLE,
 });
 
-const withConfiguredSentryConfig = (nextConfig) => withSentryConfig(nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-  
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "stackframe-pw",
-    project: "stack-server",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-  
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-  
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-  
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-  
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-  
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  },
-);
+const withConfiguredSentryConfig = (nextConfig) =>
+  withSentryConfig(
+    nextConfig,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+
+      // Suppresses source map uploading logs during build
+      silent: true,
+      org: "stackframe-pw",
+      project: "stack-server",
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+      // This can increase your server load as well as your hosting bill.
+      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+      // side errors will fail.
+      tunnelRoute: "/monitoring",
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+
+      // Enables automatic instrumentation of Vercel Cron Monitors.
+      // See the following for more information:
+      // https://docs.sentry.io/product/crons/
+      // https://vercel.com/docs/cron-jobs
+      automaticVercelMonitors: true,
+    }
+  );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -72,15 +74,18 @@ const nextConfig = {
     optimizePackageImports: ["@mui/joy"],
   },
 
-  // uncomment below to disable bundle minimization (useful for debugging prod builds)
-  /*webpack(webpackConfig) {
+  webpack(webpackConfig) {
     return {
       ...webpackConfig,
-      optimization: {
-        minimize: false
-      }
+      resolve: {
+        ...webpackConfig.resolve,
+        alias: {
+          handlebars: "handlebars/dist/handlebars.js",
+          ...webpackConfig.resolve.alias,
+        },
+      },
     };
-  },*/
+  },
 
   async headers() {
     return [
@@ -114,7 +119,9 @@ const nextConfig = {
         ],
       },
     ];
-  }
+  },
 };
 
-export default withConfiguredSentryConfig(withBundleAnalyzer(withMDX(nextConfig)));
+export default withConfiguredSentryConfig(
+  withBundleAnalyzer(withMDX(nextConfig))
+);
