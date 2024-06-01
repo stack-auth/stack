d996c388c64fe4bee60a056be1684b08a0aef87e
kabir chhabra <kchhabra276@gmail.com>
2024-06-02 02:44:51 +0530

    Fix #60: Added an option to create teams on the dashboard's "Teams" section (#62)
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>

--- packages/stack-server/src/app/(main)/(protected)/projects/[projectId]/teams/page-client.tsx
+++ packages/stack-server/src/app/(main)/(protected)/projects/[projectId]/teams/page-client.tsx
@@ -1,16 +1,61 @@
 "use client";
+import React from "react";
 import { useAdminApp } from "../use-admin-app";
 import { PageLayout } from "../page-layout";
 import { TeamTable } from "@/components/data-table/team-table";
+import { Button } from "@/components/ui/button";
+import { SmartFormDialog } from "@/components/form-dialog";
+import * as yup from "yup";
 
+type CreateDialogProps = {
+  open: boolean,
+  onOpenChange: (open: boolean) => void,
+};
 
 export default function PageClient() {
   const stackAdminApp = useAdminApp();
   const teams = stackAdminApp.useTeams();
 
+  const [createTeamsOpen, setCreateTeamsOpen] = React.useState(false);
+ 
   return (
-    <PageLayout title="Teams">
+    <PageLayout
+      title="Teams"
+      actions={
+        <Button onClick={() => setCreateTeamsOpen(true)}>
+          Create Team
+        </Button>
+      }>
       <TeamTable teams={teams} />
+      <CreateDialog
+        open={createTeamsOpen}
+        onOpenChange={setCreateTeamsOpen}
+      />
     </PageLayout>
+  );
+}
+
+function CreateDialog({ open, onOpenChange }: CreateDialogProps) {
+  const stackAdminApp = useAdminApp();
+ 
+
+  const formSchema = yup.object({
+    displayName: yup.string().required().label("Display Name"),
+  });
+
+  return (
+    <SmartFormDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      title="Create a Team"
+      formSchema={formSchema}
+      okButton={{ label: "Save" }}
+      onSubmit={async (values) => {
+        await stackAdminApp.createTeam({
+          displayName: values.displayName,
+        });
+      }}
+      cancelButton
+    />
   );
 }
