f558d6da28cf56cc5846d4d750f3f3f0ec5a98b3
Zai Shi <zaishi00@outlook.com>
2024-06-20 12:32:27 +0200

    added watch css

--- packages/stack/scripts/process-css.ts
+++ packages/stack/scripts/process-css.ts
@@ -11,7 +11,7 @@ async function main() {
   const args = process.argv.slice(2);
 
   if (args.length !== 3) {
-    console.error('Please provide exactly three arguments: input file path, output file path, and wrap string');
+    console.error('Please provide exactly three arguments: input file path, output file path, and scope name');
     process.exit(1);
   }
 
@@ -29,14 +29,20 @@ async function main() {
 
   // swap the case like .scope img to img.scope
   content = content.replace(/(\.--stack-sentinel--\s)([*a-zA-Z0-9\-]+)([^,{\n]*)/g, `$2.${scopeName}$3`)
-  
+
   // replace the remaining sentinels
   content = replaceAll(content, sentinel + ' ', scopeName);
+
+  // remove all :root
+  content = replaceAll(content, ':root', '');
 
   // double check that all sentinels were replaced
   if (content.includes(sentinel)) {
     throw new Error('Sentinel not replaced');
   }
+
+  // output css file for debugging
+  fs.writeFileSync(outputPath.replace(/\.ts$/, '.css'), content);
 
   content = JSON.stringify(content);
   content = "export const globalCSS = " + content + ";";
