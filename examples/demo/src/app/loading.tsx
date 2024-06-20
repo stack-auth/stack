a3ab7588dcb37f45d52e2c9fad330357793c79e8
Zai Shi <zaishi00@outlook.com>
2024-06-20 22:29:36 +0200

    updated demo

--- examples/demo/src/app/loading.tsx
+++ examples/demo/src/app/loading.tsx
@@ -1,6 +1,3 @@
 export default function Loading() {
-  // return <div className="flex items-center justify-center h-full">
-  //   <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-500"></div>
-  // </div>;
   return null;
 }
