19ca6261e46ced1c32260dcd04cae06585ca01ff
Zai Shi <zaishi00@outlook.com>
2024-06-24 11:22:42 +0200

    fixed demo dark mode

--- examples/demo/tailwind.config.js
+++ examples/demo/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["selector", '[data-stack-theme="dark"]'],
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@stackframe/stack-ui/src/**/*.{ts,tsx}",
