3f6c8c4ad2fa299b98fae7dc05de1c2f3884972a
Stan Wohlwend <n2d4xc@gmail.com>
2024-07-05 17:27:05 -0700

    Queries

--- apps/backend/src/route-handlers/prisma-handler.tsx
+++ apps/backend/src/route-handlers/prisma-handler.tsx
@@ -19,17 +19,18 @@ type PUpdate<T extends AllPrismaModelNames> = Prisma.TypeMap["model"][Capitalize
 type PCreate<T extends AllPrismaModelNames> = Prisma.TypeMap["model"][Capitalize<T>]["operations"]["create"]["args"]["data"];
 type PEitherWrite<T extends AllPrismaModelNames> = (PCreate<T> | PUpdate<T>) & Partial<(PCreate<T> | {}) & (PUpdate<T> | {})>;
 
-type Context<AllParams extends boolean, PS extends ParamsSchema> = {
-  params: [AllParams] extends [true] ? yup.InferType<PS> : Partial<yup.InferType<PS>>,
-  auth: SmartRequestAuth,
-};
-
-type CrudToPrismaContext<AllParams extends boolean, PS extends ParamsSchema> = Context<AllParams, PS> & { type: 'update' | 'create' };
-
 type CRead<T extends CrudTypeOf<any>> = T extends { Admin: { Read: infer R } } ? R : never;
 type CCreate<T extends CrudTypeOf<any>> = T extends { Admin: { Create: infer R } } ? R : never;
 type CUpdate<T extends CrudTypeOf<any>> = T extends { Admin: { Update: infer R } } ? R : never;
 type CEitherWrite<T extends CrudTypeOf<any>> = (CCreate<T> | CUpdate<T>) & Partial<CCreate<T> & CUpdate<T>>;
+
+
+type Context<AllParams extends boolean, PS extends ParamsSchema, QS extends QuerySchema> = {
+  params: [AllParams] extends [true] ? yup.InferType<PS> : Partial<yup.InferType<PS>>,
+  auth: SmartRequestAuth,
+  query: yup.InferType<QS>,
+};
+type CrudToPrismaContext<AllParams extends boolean, PS extends ParamsSchema, QS extends QuerySchema> = Context<AllParams, PS, QS> & { type: 'update' | 'create' };
 
 type CrudHandlersFromCrudType<T extends CrudTypeOf<CrudSchema>, PS extends ParamsSchema, QS extends QuerySchema> = CrudHandlers<
   T,
@@ -46,12 +47,13 @@ type ExtraDataFromCrudType<
   S extends CrudSchema,
   PrismaModelName extends AllPrismaModelNames,
   PS extends ParamsSchema,
+  QS extends QuerySchema,
   W extends Where<PrismaModelName>,
   I extends Include<PrismaModelName>,
   B extends BaseFields<PrismaModelName>,
 > = {
-  getInclude(params: yup.InferType<PS>, context: Pick<Context<false, PS>, "auth">): Promise<I>,
-  transformPrismaToCrudObject(prismaOrNull: PRead<PrismaModelName, W & B, I> | null, params: yup.InferType<PS>, context: Pick<Context<false, PS>, "auth">): Promise<CRead<CrudTypeOf<S>>>,
+  getInclude(params: yup.InferType<PS>, query: yup.InferType<QS>, context: Pick<Context<false, PS, QS>, "auth">): Promise<I>,
+  transformPrismaToCrudObject(prismaOrNull: PRead<PrismaModelName, W & B, I> | null, params: yup.InferType<PS>, query: yup.InferType<QS>, context: Pick<Context<false, PS, QS>, "auth">): Promise<CRead<CrudTypeOf<S>>>,
 };
 
 export function createPrismaCrudHandlers<
@@ -68,28 +70,29 @@ export function createPrismaCrudHandlers<
   options: & {
       paramsSchema: PS,
       querySchema?: QS,
-      onPrepare?: (context: Context<false, PS>) => Promise<void>,
-      baseFields: (context: Context<false, PS>) => Promise<B>,
-      where?: (context: Context<false, PS>) => Promise<W>,
-      whereUnique?: (context: Context<true, PS>) => Promise<WhereUnique<PrismaModelName>>,
-      include: (context: Context<false, PS>) => Promise<I>,
-      crudToPrisma: (crud: CEitherWrite<CrudTypeOf<S>>, context: CrudToPrismaContext<false, PS>) => Promise<PEitherWrite<PrismaModelName>>,
-      prismaToCrud: (prisma: PRead<PrismaModelName, W & B, I>, context: Context<false, PS>) => Promise<CRead<CrudTypeOf<S>>>,
-      notFoundToCrud: (context: Context<false, PS>) => Promise<CRead<CrudTypeOf<S>> | never>,
-      onCreate?: (prisma: PRead<PrismaModelName, W & B, I>, context: Context<false, PS>) => Promise<void>,
+      onPrepare?: (context: Context<false, PS, QS>) => Promise<void>,
+      baseFields: (context: Context<false, PS, QS>) => Promise<B>,
+      where?: (context: Context<false, PS, QS>) => Promise<W>,
+      whereUnique?: (context: Context<true, PS, QS>) => Promise<WhereUnique<PrismaModelName>>,
+      include: (context: Context<false, PS, QS>) => Promise<I>,
+      crudToPrisma: (crud: CEitherWrite<CrudTypeOf<S>>, context: CrudToPrismaContext<false, PS, QS>) => Promise<PEitherWrite<PrismaModelName>>,
+      prismaToCrud: (prisma: PRead<PrismaModelName, W & B, I>, context: Context<false, PS, QS>) => Promise<CRead<CrudTypeOf<S>>>,
+      notFoundToCrud: (context: Context<false, PS, QS>) => Promise<CRead<CrudTypeOf<S>> | never>,
+      onCreate?: (prisma: PRead<PrismaModelName, W & B, I>, context: Context<false, PS, QS>) => Promise<void>,
     },
-): CrudHandlersFromCrudType<CrudTypeOf<S>, PS, QS> & ExtraDataFromCrudType<S, PrismaModelName, PS, W, I, B> {
-  const wrapper = <AllParams extends boolean, T>(allParams: AllParams, func: (data: any, context: Context<AllParams, PS>) => Promise<T>): (opts: Context<AllParams, PS> & { data?: unknown }) => Promise<T> => {
+): CrudHandlersFromCrudType<CrudTypeOf<S>, PS, QS> & ExtraDataFromCrudType<S, PrismaModelName, PS, QS, W, I, B> {
+  const wrapper = <AllParams extends boolean, T>(allParams: AllParams, func: (data: any, context: Context<AllParams, PS, QS>) => Promise<T>): (opts: Context<AllParams, PS, QS> & { data?: unknown }) => Promise<T> => {
     return async (req) => {
-      const context: Context<AllParams, PS> = {
+      const context: Context<AllParams, PS, QS> = {
         params: req.params,
         auth: req.auth,
+        query: req.query,
       };
       return await func(req.data, context);
     };
   };
 
-  const prismaOrNullToCrud = (prismaOrNull: PRead<PrismaModelName, W & B, I> | null, context: Context<false, PS>) => {
+  const prismaOrNullToCrud = (prismaOrNull: PRead<PrismaModelName, W & B, I> | null, context: Context<false, PS, QS>) => {
     if (prismaOrNull === null) {
       return options.notFoundToCrud(context);
     } else {
@@ -183,17 +186,19 @@ export function createPrismaCrudHandlers<
       }
     }),
   }), {
-    getInclude(params, context) {
+    getInclude(params, query, context) {
       return options.include({
         ...context,
         params,
+        query,
       });
     },
-    transformPrismaToCrudObject(prismaOrNull, params, context) {
+    transformPrismaToCrudObject(prismaOrNull, params, query, context) {
       return prismaOrNullToCrud(prismaOrNull, {
         ...context,
         params,
+        query,
       });
     },
-  } satisfies ExtraDataFromCrudType<S, PrismaModelName, PS, W, I, B>);
+  } satisfies ExtraDataFromCrudType<S, PrismaModelName, PS, QS, W, I, B>);
 }
