4c4c6ad28c4bb66310da602dd59f60de61fb83fd
Zai Shi <zaishi00@outlook.com>
2024-07-11 19:21:04 -0700

    added more tests

--- apps/e2e/tests/backend/endpoints/api/v1/team-permission-definitions.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/team-permission-definitions.test.ts
@@ -2,7 +2,7 @@ import { it } from "../../../../helpers";
 import { ApiKey, Auth, InternalProjectKeys, Project, Team, backendContext, niceBackendFetch } from "../../../backend-helpers";
 
 
-it("lists all the team permissions the current project have", async ({ expect }) => {
+it("lists all the team permissions", async ({ expect }) => {
   backendContext.set({
     projectKeys: InternalProjectKeys,
   });
@@ -50,6 +50,33 @@ it("lists all the team permissions the current project have", async ({ expect })
             "id": "$invite_members",
           },
         ],
+      },
+      "headers": Headers { <some fields may have been hidden> },
+    }
+  `);
+});
+
+it("create a new team permission", async ({ expect }) => {
+  backendContext.set({
+    projectKeys: InternalProjectKeys,
+  });
+  const { adminAccessToken } = await Project.createAndSetAdmin();
+  await ApiKey.createAndSetProjectKeys(adminAccessToken);
+
+  const response = await niceBackendFetch(`/api/v1/team-permission-definitions`, {
+    accessType: "server",
+    method: "POST",
+    body: {
+      id: 'new_permission'
+    }
+  });
+  expect(response).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 201,
+      "body": {
+        "__database_id": "<stripped UUID>",
+        "contained_permission_ids": [],
+        "id": "new_permission",
       },
       "headers": Headers { <some fields may have been hidden> },
     }
