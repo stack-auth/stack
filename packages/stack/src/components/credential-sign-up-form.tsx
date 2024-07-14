58d80d846e5634252b682f9586269a7f3530efa9
Stan Wohlwend <n2d4xc@gmail.com>
2024-07-13 19:53:25 -0700

    Credential sign up form now loads correctly

--- packages/stack/src/components/credential-sign-up-form.tsx
+++ packages/stack/src/components/credential-sign-up-form.tsx
@@ -81,7 +81,7 @@ export function CredentialSignUpForm() {
       />
       <FormWarningText text={errors.passwordRepeat?.message?.toString()} />
       
-      <Button type="submit" className="mt-6">
+      <Button type="submit" className="mt-6" loading={loading}>
         Sign Up
       </Button>
     </form>
