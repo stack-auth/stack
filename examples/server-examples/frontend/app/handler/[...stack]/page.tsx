8512d68114ebb791f793e0d78c3b64c267f750e5
Zai Shi <zaishi00@outlook.com>
2024-07-23 21:59:10 -0700

    added server example docs

--- examples/server-examples/frontend/app/handler/[...stack]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { StackHandler } from "@stackframe/stack";
-import { stackServerApp } from "../../../stack";
-
-export default function Handler(props: any) {
-  return <StackHandler fullPage app={stackServerApp} {...props} />;
-}
