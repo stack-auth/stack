650a47c72f91e415ceba9175d246c3190dcf548b
Zai Shi <zaishi00@outlook.com>
2024-07-26 15:18:10 -0700

    added shared spotify

--- packages/stack-shared/src/utils/oauth.tsx
+++ packages/stack-shared/src/utils/oauth.tsx
@@ -1,5 +1,5 @@
 export const standardProviders = ["google", "github", "facebook", "microsoft", "spotify"] as const;
-export const sharedProviders = ["google", "github", "facebook", "microsoft"] as const;
+export const sharedProviders = ["google", "github", "facebook", "microsoft", "spotify"] as const;
 export const allProviders = ["google", "github", "facebook", "microsoft", "spotify"] as const;
 
 export type ProviderType = typeof allProviders[number];
