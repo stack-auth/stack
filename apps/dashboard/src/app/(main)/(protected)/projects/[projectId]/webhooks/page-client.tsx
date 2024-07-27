5b86682cc3713dcb584df4c2e432026f3147f1c6
Zai Shi <zaishi00@outlook.com>
2024-07-27 10:42:30 -0700

    added more info on the webhook page

--- apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/webhooks/page-client.tsx
+++ apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/webhooks/page-client.tsx
@@ -6,6 +6,8 @@ import { useAdminApp } from "../use-admin-app";
 import { SvixProvider, useEndpoints, useSvix } from "svix-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ActionCell } from "@/components/data-table/elements/cells";
+import { SettingCard } from "@/components/settings";
+import { Button } from "@/components/ui/button";
 
 function ActionMenu() {
   return (
@@ -24,41 +26,59 @@ function ActionMenu() {
 function Endpoints() {
   const endpoints = useEndpoints();
   const svix = useSvix();
+  let content = null;
 
   if (endpoints.error) {
-    return <Alert>An error has occurred</Alert>;
+    content = <Alert>An error has occurred</Alert>;
   }
 
   if (endpoints.loading) {
-    return <Alert>Loading...</Alert>;
+    content = <Alert>Loading...</Alert>;
   }
 
   if (!endpoints.data?.length) {
-    return <Alert>No domains added yet.</Alert>;
+    content = <Alert>No domains added yet.</Alert>;
+  } else {
+    content = (
+      <div className="border rounded-md">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[600px]">Endpoint URL</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {endpoints.data.map(endpoint => (
+              <TableRow key={endpoint.id}>
+                <TableCell>{endpoint.url}</TableCell>
+                <TableCell className="flex justify-end gap-4">
+                  <ActionMenu />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    );
   }
 
-
   return (
-    <div className="border rounded-md">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[600px]">Endpoint URL</TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {endpoints.data.map(endpoint => (
-            <TableRow key={endpoint.id}>
-              <TableCell>{endpoint.url}</TableCell>
-              <TableCell className="flex justify-end gap-4">
-                <ActionMenu />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <SettingCard
+      title="Endpoints"
+      description="Endpoints are the URLs that we will send events to. Please make sure you control these endpoints, as they can receive sensitive data."
+      actions={<Button>Add new endpoint</Button>}
+      // actions={
+      //   <EditDialog
+      //     trigger={<Button>Add new domain</Button>}
+      //     domains={domains}
+      //     project={project}
+      //     type="create"
+      //   />
+      // }
+    >
+      {content}
+    </SettingCard>
   );
 }
 
@@ -68,7 +88,10 @@ export default function PageClient() {
   const svixToken = stackAdminApp.useSvixToken();
 
   return (
-    <PageLayout title="Webhooks">
+    <PageLayout
+      title="Webhooks"
+      description="Webhooks are used to sync users and teams events from Stack to your own server."
+    >
       <SvixProvider
         token={svixToken}
         appId={stackAdminApp.projectId}
