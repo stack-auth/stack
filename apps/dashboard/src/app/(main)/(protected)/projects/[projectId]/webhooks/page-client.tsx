d36e6996b453837cd6f2a7338a151c82f15c0239
Zai Shi <zaishi00@outlook.com>
2024-07-27 13:33:31 -0700

    added create endpoints dialog

--- apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/webhooks/page-client.tsx
+++ apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/webhooks/page-client.tsx
@@ -3,11 +3,43 @@
 import { Alert } from "@/components/ui/alert";
 import { PageLayout } from "../page-layout";
 import { useAdminApp } from "../use-admin-app";
-import { SvixProvider, useEndpoints, useSvix } from "svix-react";
+import { SvixProvider, useEndpoints, useNewEndpoint } from "svix-react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ActionCell } from "@/components/data-table/elements/cells";
 import { SettingCard } from "@/components/settings";
 import { Button } from "@/components/ui/button";
+import * as yup from "yup";
+import { urlSchema } from "@stackframe/stack-shared/dist/schema-fields";
+import { SmartFormDialog } from "@/components/form-dialog";
+
+function CreateDialog(props: {
+  trigger?: React.ReactNode,
+}) {
+  const { createEndpoint, url } = useNewEndpoint();
+
+  const domainFormSchema = yup.object({
+    makeSureAlert: yup.mixed().meta({
+      stackFormFieldRender: () => (
+        <Alert>
+          Make sure this is a trusted URL that you control.
+        </Alert>
+      ),
+    }),
+    url: urlSchema.required().label("URL"),
+  });
+
+  return <SmartFormDialog
+    trigger={props.trigger}
+    title={"Create new endpoint"}
+    formSchema={domainFormSchema}
+    okButton={{ label: "Create" }}
+    onSubmit={async (values) => {
+      url.setValue(values.url);
+      const out = await createEndpoint();
+      console.log(out);
+    }}
+  />;
+}
 
 function ActionMenu() {
   return (
@@ -25,7 +57,6 @@ function ActionMenu() {
 
 function Endpoints() {
   const endpoints = useEndpoints();
-  const svix = useSvix();
   let content = null;
 
   if (endpoints.error) {
@@ -67,15 +98,7 @@ function Endpoints() {
     <SettingCard
       title="Endpoints"
       description="Endpoints are the URLs that we will send events to. Please make sure you control these endpoints, as they can receive sensitive data."
-      actions={<Button>Add new endpoint</Button>}
-      // actions={
-      //   <EditDialog
-      //     trigger={<Button>Add new domain</Button>}
-      //     domains={domains}
-      //     project={project}
-      //     type="create"
-      //   />
-      // }
+      actions={<CreateDialog trigger={<Button>Add new endpoint</Button>}/>}
     >
       {content}
     </SettingCard>
