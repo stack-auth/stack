1128e59b6cbd6fea8bf42d57e3b05493222e5cf0
Stan Wohlwend <n2d4xc@gmail.com>
2024-07-27 11:45:33 -0700

    Improved proxy config

--- apps/dashboard/src/app/providers.tsx
+++ apps/dashboard/src/app/providers.tsx
@@ -5,12 +5,11 @@ import { PostHogProvider } from 'posthog-js/react';
 import { Suspense, useEffect, useState } from 'react';
 
 if (typeof window !== 'undefined') {
-  if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: 'identified_only',
-    });
-  }
+  posthog.init("phc_vIUFi0HzHo7oV26OsaZbUASqxvs8qOmap1UBYAutU4k", {
+    api_host: "/consume",
+    ui_host: "https://eu.i.posthog.com",
+    person_profiles: 'identified_only',
+  });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
