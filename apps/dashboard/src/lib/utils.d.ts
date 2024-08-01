7631afdf261361a4b640d6b50c68def587f637cd
Zai Shi <zaishi00@outlook.com>
2024-08-01 15:50:59 -0700

    removed unused files

--- apps/dashboard/src/lib/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { type ClassValue } from "clsx";
-import { DomainConfigJson } from "@/temporary-types";
-export declare function cn(...inputs: ClassValue[]): string;
-export declare function validateUrl(url: string, domains: DomainConfigJson[], allowLocalhost: boolean): boolean;
-//# sourceMappingURL=utils.d.ts.map
