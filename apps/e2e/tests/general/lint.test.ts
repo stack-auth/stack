94a3edd77de0dc8056018cbb613fe79bfa7b7eaf
Konsti Wohlwend <n2d4xc@gmail.com>
2024-08-03 21:05:24 -0700

    Svix concurrency workaround (#167)

--- apps/e2e/tests/general/lint.test.ts
+++ apps/e2e/tests/general/lint.test.ts
@@ -10,5 +10,5 @@ describe.todo("`pnpm run lint`", () => {
       });
     });
     expect(error, `Expected no error to be thrown!\n\n\n\nstdout: ${stdout}\n\n\n\nstderr: ${stderr}`).toBeNull();
-  }, 60_000);
+  }, 120_000);
 });
--- packages/stack-shared/src/utils/uuids.tsx
+++ packages/stack-shared/src/utils/uuids.tsx
@@ -1,9 +1,9 @@
-import { globalVar } from "./globals";
+import { generateRandomValues } from "./crypto";
 
 export function generateUuid() {
   // crypto.randomUuid is not supported in all browsers, so this is a polyfill
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ globalVar.crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+    (+c ^ generateRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
   );
 }
 
