d5cfe52b3dcb7b7fb4d4eaefadf2aa8fc24dcec3
Zai Shi <zaishi00@outlook.com>
2024-08-04 13:45:11 -0700

    added more team invitation test

--- apps/e2e/tests/backend/endpoints/api/v1/team-invitations.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/team-invitations.test.ts
@@ -28,7 +28,7 @@ it("is not allowed to send invitation without permission", async ({ expect }) =>
   `);
 });
 
-it("should verify user's email", async ({ expect }) => {
+it("invites a user to a team", async ({ expect }) => {
   const { userId: userId1 } = await Auth.Otp.signIn();
   const { teamId } = await Team.create();
 
@@ -52,7 +52,29 @@ it("should verify user's email", async ({ expect }) => {
   `);
 
   backendContext.set({ mailbox: receiveMailbox });
-  const { userId: userId2 } = await Auth.Otp.signIn();
+  await Auth.Otp.signIn();
 
   await Team.acceptInvitation();
+
+  const response = await niceBackendFetch(`/api/v1/teams?user_id=me`, {
+    accessType: "server",
+    method: "GET",
+  });
+  expect(response).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 200,
+      "body": {
+        "is_paginated": false,
+        "items": [
+          {
+            "created_at_millis": <stripped field 'created_at_millis'>,
+            "display_name": "New Team",
+            "id": "<stripped UUID>",
+            "profile_image_url": null,
+          },
+        ],
+      },
+      "headers": Headers { <some fields may have been hidden> },
+    }
+  `);
 });
