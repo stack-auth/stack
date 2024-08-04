14ce02bd843a63bab5f9222f101af3cbe2f0bf52
Zai Shi <zaishi00@outlook.com>
2024-08-03 17:56:10 -0700

    fixed team invitation page

--- packages/stack/src/components-page/team-invitation.tsx
+++ packages/stack/src/components-page/team-invitation.tsx
@@ -1,19 +1,88 @@
 'use client';
 
+import { KnownErrors } from "@stackframe/stack-shared";
 import { cacheFunction } from "@stackframe/stack-shared/dist/utils/caches";
-import { Button, Typography } from "@stackframe/stack-ui";
+import { runAsynchronouslyWithAlert } from "@stackframe/stack-shared/dist/utils/promises";
+import { Typography } from "@stackframe/stack-ui";
 import React from "react";
 import { MessageCard, StackClientApp, useStackApp, useUser } from "..";
-import { MaybeFullPage } from "../components/elements/maybe-full-page";
-import { runAsynchronouslyWithAlert } from "@stackframe/stack-shared/dist/utils/promises";
+import { PredefinedMessageCard } from "../components/message-cards/predefined-message-card";
 
 const cachedVerifyInvitation = cacheFunction(async (stackApp: StackClientApp<true>, code: string) => {
   return await stackApp.verifyTeamInvitationCode(code);
 });
 
-export function TeamInvitation({ fullPage=false, searchParams }: { fullPage?: boolean, searchParams: Record<string, string> }) {
+function TeamInvitationInner(props: { fullPage?: boolean, searchParams: Record<string, string>, data: { teamDisplayName: string } }) {
   const stackApp = useStackApp();
+  const [success, setSuccess] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+
+  if (success) {
+    return (
+      <MessageCard
+        title="Team invitation"
+        fullPage={props.fullPage}
+        primaryButtonText="Go to home"
+        primaryAction={() => stackApp.redirectToHome()}
+      >
+        <Typography>You have successfully joined {props.data.teamDisplayName}</Typography>
+      </MessageCard>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+      <PredefinedMessageCard type="unknownError" fullPage={props.fullPage} />
+    );
+  }
+
+  return (
+    <MessageCard
+      title="Team invitation"
+      fullPage={props.fullPage}
+      primaryButtonText="Join"
+      primaryAction={() => runAsynchronouslyWithAlert(async () => {
+        const result = await stackApp.acceptTeamInvitation(props.searchParams.code || '');
+        if (result.status === 'error') {
+        setErrorMessage(result.error.message);
+        } else {
+        setSuccess(true);
+        }
+      })}
+      secondaryButtonText="Ignore"
+      secondaryAction={() => stackApp.redirectToHome()}
+    >
+      <Typography>You are invited to join {props.data.teamDisplayName}</Typography>
+    </MessageCard>
+  );
+}
+
+export function TeamInvitation({ fullPage=false, searchParams }: { fullPage?: boolean, searchParams: Record<string, string> }) {
   const user = useUser();
+  const stackApp = useStackApp();
+
+  const invalidJsx = (
+    <MessageCard title="Invalid Team Invitation Link" fullPage={fullPage}>
+      <Typography>Please double check if you have the correct team invitation link.</Typography>
+    </MessageCard>
+  );
+
+  const expiredJsx = (
+    <MessageCard title="Expired Team Invitation Link" fullPage={fullPage}>
+      <Typography>Your team invitation link has expired. Please request a new team invitation link </Typography>
+    </MessageCard>
+  );
+
+  const usedJsx = (
+    <MessageCard title="Used Team Invitation Link" fullPage={fullPage}>
+      <Typography>This team invitation link has already been used.</Typography>
+    </MessageCard>
+  );
+
+  const code = searchParams.code;
+  if (!code) {
+    return invalidJsx;
+  }
 
   if (!user) {
     return (
@@ -33,26 +102,17 @@ export function TeamInvitation({ fullPage=false, searchParams }: { fullPage?: bo
   const verificationResult = React.use(cachedVerifyInvitation(stackApp, searchParams.code || ''));
 
   if (verificationResult.status === 'error') {
-    return (
-      <MessageCard
-        title="Invalid Invitation"
-        fullPage={fullPage}
-        primaryButtonText="Go to home"
-        primaryAction={() => stackApp.redirectToHome()}
-      />
-    );
+    const error = verificationResult.error;
+    if (error instanceof KnownErrors.VerificationCodeNotFound) {
+      return invalidJsx;
+    } else if (error instanceof KnownErrors.VerificationCodeExpired) {
+      return expiredJsx;
+    } else if (error instanceof KnownErrors.VerificationCodeAlreadyUsed) {
+      return usedJsx;
+    } else {
+      throw error;
+    }
   }
 
-  return (
-    <MessageCard
-      title="Team invitation"
-      fullPage={fullPage}
-      primaryButtonText="Join"
-      primaryAction={() => runAsynchronouslyWithAlert(stackApp.acceptTeamInvitation(searchParams.code || ''))}
-      secondaryButtonText="Ignore"
-      secondaryAction={() => stackApp.redirectToHome()}
-    >
-      <Typography>You are invited to join {verificationResult.data.teamDisplayName}</Typography>
-    </MessageCard>
-  );
+  return <TeamInvitationInner fullPage={fullPage} searchParams={searchParams} data={verificationResult.data} />;
 };
