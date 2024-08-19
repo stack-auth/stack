43799fd3d9ba8c7d117cf8ca3aa12c795df56a67
Zai Shi <zaishi00@outlook.com>
2024-08-19 10:39:38 -0700

    added more in the feature section

--- examples/template/app/(landing-page)/page.tsx
+++ examples/template/app/(landing-page)/page.tsx
@@ -2,26 +2,27 @@ import { FeatureGrid } from "@/components/features";
 import { Hero } from "@/components/hero";
 import { PricingGrid } from "@/components/pricing";
 import { stackServerApp } from "@/stack";
-import { ShieldCheck, Users } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Component, ComponentIcon, ShieldCheck, Users } from "lucide-react";
 
 export default async function IndexPage() {
   return (
     <>
       <Hero
-        capsuleText="Next + Shadcn + Stack Auth"
+        capsuleText="100% Open-source & Free"
         capsuleLink="https://stacktemplate.com"
         title="A Minimalistic Next.js Starter Template"
-        subtitle="A Next.js 13 app dir template."
+        subtitle="A full-stack template built with Next.js, Shadcn UI, and Stack Auth. Modern, modular, and open-source."
         primaryCtaText="Get Started"
         primaryCtaLink="/docs"
         secondaryCtaText="GitHub"
         secondaryCtaLink="#"
       />
 
-      <div id='features' />
+      <div id="features" />
       <FeatureGrid
         title="Features"
-        subtitle="This project is an experiment to see how a modern app, with features like auth, subscriptions, API routes, and static pages would work in Next.js 13 app dir."
+        subtitle="A few of the things you can do with Stack Template."
         items={[
           {
             icon: (
@@ -33,19 +34,79 @@ export default async function IndexPage() {
             description: "App router, Layouts, and Suspense.",
           },
           {
-            icon: <ShieldCheck className="h-12 w-12" />,
-            title: "Authentication",
-            description: "Magic links, email/password, and OAuth.",
+            icon: (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 256"
+                className="h-12 w-12 fill-current"
+              >
+                <rect width="256" height="256" fill="none"></rect>
+                <line
+                  x1="208"
+                  y1="128"
+                  x2="128"
+                  y2="208"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="22"
+                ></line>
+                <line
+                  x1="192"
+                  y1="40"
+                  x2="40"
+                  y2="192"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="22"
+                ></line>
+              </svg>
+            ),
+            title: "Shadcn UI",
+            description: "Dark mode, layout components, and more.",
+          },
+          {
+            icon: (
+              <svg
+                width="201"
+                height="242"
+                viewBox="0 0 201 242"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-12 w-12 fill-current"
+              >
+                <path
+                  d="M104.004 1.78785C101.751 0.662376 99.1002 0.663161 96.8483 1.78998L4.9201 47.7892C2.21103 49.1448 0.5 51.9143 0.5 54.9436V130.526C0.5 133.556 2.2123 136.327 4.92292 137.682L96.9204 183.67C99.1725 184.796 101.823 184.796 104.075 183.67L168.922 151.246C174.242 148.587 180.5 152.455 180.5 158.402V168.855C180.5 171.885 178.788 174.655 176.078 176.01L104.077 212.011C101.825 213.137 99.1745 213.137 96.9224 212.012L12.0771 169.598C6.75791 166.939 0.5 170.807 0.5 176.754V187.048C0.5 190.083 2.21689 192.856 4.93309 194.209L97.0051 240.072C99.2529 241.191 101.896 241.191 104.143 240.07L196.071 194.21C198.785 192.857 200.5 190.084 200.5 187.052V119.487C200.5 113.54 194.242 109.672 188.922 112.332L132.078 140.754C126.758 143.414 120.5 139.546 120.5 133.599V123.145C120.5 120.115 122.212 117.345 124.922 115.99L196.078 80.4124C198.788 79.0573 200.5 76.2872 200.5 73.257V54.9468C200.5 51.9158 198.787 49.1451 196.076 47.7904L104.004 1.78785Z"
+                />
+              </svg>
+            ),
+            title: "Stack Auth",
+            description: "Login, Sign up, OAuth, User management, and more.",
           },
           {
             icon: <Users className="h-12 w-12" />,
             title: "Multi-tenancy",
             description: "Teams and permissions out of the box.",
           },
+          {
+            icon: <GitHubLogoIcon className="h-12 w-12" />,
+            title: "100% Open-source",
+            description:
+              "The template and the auth server are both open-source and self-hostable.",
+          },
+          {
+            icon: <ComponentIcon className="h-12 w-12" />,
+            title: "Modular",
+            description:
+              "Easily extendable and customizable. No spaghetti copy-paste code.",
+          },
         ]}
       />
 
-      <div id='pricing' />
+      <div id="pricing" />
       <PricingGrid
         title="Pricing"
         subtitle="Choose a plan that works for you."
@@ -54,7 +115,13 @@ export default async function IndexPage() {
             title: "Basic",
             price: "Free",
             description: "Perfect for individuals and small projects",
-            features: ["Full source code", "100% Open-source", "Community support", "Free forever", "No credit card required"],
+            features: [
+              "Full source code",
+              "100% Open-source",
+              "Community support",
+              "Free forever",
+              "No credit card required",
+            ],
             buttonText: "Get Started",
             buttonHref: stackServerApp.urls.signUp,
           },
@@ -62,7 +129,13 @@ export default async function IndexPage() {
             title: "Pro",
             price: "$0.00",
             description: "Ideal for growing teams and businesses",
-            features: ["Full source code", "100% Open-source", "Community support", "Free forever", "No credit card required"],
+            features: [
+              "Full source code",
+              "100% Open-source",
+              "Community support",
+              "Free forever",
+              "No credit card required",
+            ],
             buttonText: "Upgrade to Pro",
             isPopular: true,
             buttonHref: stackServerApp.urls.signUp,
@@ -71,7 +144,13 @@ export default async function IndexPage() {
             title: "Enterprise",
             price: "Still Free",
             description: "For large organizations",
-            features: ["Full source code", "100% Open-source", "Community support", "Free forever", "No credit card required"],
+            features: [
+              "Full source code",
+              "100% Open-source",
+              "Community support",
+              "Free forever",
+              "No credit card required",
+            ],
             buttonText: "Contact Us",
             buttonHref: stackServerApp.urls.signUp,
           },
