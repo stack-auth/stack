3f4015e9561aee82443144c499345661f6b5d2d9
Zai Shi <zaishi00@outlook.com>
2024-08-21 20:14:10 -0700

    removed v1 api

--- apps/dashboard/src/app/api/v1/users/[userId]/route.tsx
+++
@@ -1,5 +0,0 @@
-import { usersCrudHandlers } from "../crud";
-
-export const GET = usersCrudHandlers.readHandler;
-export const PUT = usersCrudHandlers.updateHandler;
-export const DELETE = usersCrudHandlers.deleteHandler;
