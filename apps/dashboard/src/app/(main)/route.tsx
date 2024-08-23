346b52d54374dde7b5744bcfb4668ce9c4d32ab5
Zai Shi <zaishi00@outlook.com>
2024-08-22 18:22:11 -0700

    fixed import errors

--- apps/dashboard/src/app/(main)/route.tsx
+++
@@ -1,6 +0,0 @@
-import { deprecatedSmartRouteHandler } from "@/route-handlers/smart-route-handler";
-import { redirect } from "next/navigation";
-
-export const GET = deprecatedSmartRouteHandler(async () => {
-  redirect("/projects");
-});
