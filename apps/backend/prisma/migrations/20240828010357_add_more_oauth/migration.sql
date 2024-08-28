c6e375255c707fca694cf071f236e06133adf5dc
Manoj Kumar <ms8460149@gmail.com>
2024-08-28 06:34:25 +0530

    add migration

---
+++ apps/backend/prisma/migrations/20240828010357_add_more_oauth/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "StandardOAuthProviderType" ADD VALUE 'ZOOM';
+ALTER TYPE "StandardOAuthProviderType" ADD VALUE 'DROPBOX';
+ALTER TYPE "StandardOAuthProviderType" ADD VALUE 'COINBASE';
+ALTER TYPE "StandardOAuthProviderType" ADD VALUE 'INSTAGRAM';
+ALTER TYPE "StandardOAuthProviderType" ADD VALUE 'NETLIFY';
