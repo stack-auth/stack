7e622e7d9e57fb145566ccf6605d1b9d8d968438
Zai Shi <zaishi00@outlook.com>
2024-10-10 06:35:59 +0200

    Auto detect https for cookies (#296)
    * added auto https detection
    
    * not catch all
    
    * improved cookie logic
    
    * fixed bugs
    
    * Update packages/stack/src/lib/cookie.ts
    
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>
    
    * Update packages/stack/src/lib/cookie.ts
    
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>
    
    * improve logic flow
    
    ---------
    
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>

--- packages/stack-sc/src/index.combined.ts
+++ packages/stack-sc/src/index.combined.ts
@@ -2,4 +2,5 @@ import * as def from "./index.default";
 import * as reactServer from "./index.react-server";
 
 export declare const cookies: typeof reactServer.cookies | typeof def.cookies;
+export declare const headers: typeof reactServer.headers | typeof def.headers;
 export declare const isReactServer: boolean;
