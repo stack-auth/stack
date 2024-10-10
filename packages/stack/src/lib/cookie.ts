bd6f1e491457d0cab2ef8da93567b7082d512809
Zai Shi <zaishi00@outlook.com>
2024-10-09 20:47:20 -0700

    improved cookie logic

--- packages/stack/src/lib/cookie.ts
+++ packages/stack/src/lib/cookie.ts
@@ -4,31 +4,14 @@ import { calculatePKCECodeChallenge, generateRandomCodeVerifier, generateRandomS
 
 type SetCookieOptions = { maxAge?: number };
 
-function isRscCookieUnavailableError(e: any) {
-  const allowedMessageSnippets = ["was called outside a request scope", "cookies() expects to have requestAsyncStorage"];
-  return typeof e?.message === "string" && allowedMessageSnippets.some(msg => e.message.includes(msg));
-}
-
-function isRscHeadersUnavailableError(e: any) {
-  const allowedMessageSnippets = ["was called outside a request scope"];
-  return typeof e?.message === "string" && allowedMessageSnippets.some(msg => e.message.includes(msg));
-}
-
 export function getCookie(name: string): string | null {
-  try {
-    return rscCookies().get(name)?.value ?? null;
-  } catch (e: any) {
-    if (isRscCookieUnavailableError(e)) {
-      return Cookies.get(name) ?? null;
-    } else {
-      throw e;
-    }
-  } finally {
+  if (typeof window !== "undefined") {
     // This is a flag to automatically detect whether we're on https for the next server request
     // Check out the comment in setCookie for more details
-    if (typeof window !== "undefined" && window.location.protocol === "https:") {
-      Cookies.set("stack-is-https", "true");
-    }
+    Cookies.set("stack-is-https", "true");
+    return Cookies.get(name) ?? null;
+  } else {
+    return rscCookies().get(name)?.value ?? null;
   }
 }
 
@@ -41,56 +24,38 @@ export function setOrDeleteCookie(name: string, value: string | null, options: S
 }
 
 export function deleteCookie(name: string) {
-  try {
+  if (typeof window !== "undefined") {
+    Cookies.remove(name);
+  } else {
     rscCookies().delete(name);
-  } catch (e: any) {
-    if (isRscCookieUnavailableError(e)) {
-      Cookies.remove(name);
-    } else {
-      throw e;
-    }
   }
 }
 
 export function setCookie(name: string, value: string, options: SetCookieOptions = {}) {
-  // ================================
-  // Check if the current page is server over HTTPS with
-  // 1. Check if the https cookie is set on the client
+  // Check if the current page is server over HTTPS with:
+  // 1. Check if the https cookie is set on the client (see comment above)
   // 2. If on the client, check the protocol directly
   // 3. If on the server, check the X-Forwarded-Proto header
   let isSecureCookie = !!getCookie("stack-is-https");
-  if (typeof window !== "undefined" && window.location.protocol === "https:") {
-    isSecureCookie = true;
-  }
-  try {
-    const proto = rscHeaders().get("x-forwarded-proto");
-    if (proto === "https") {
+
+  if (typeof window !== "undefined") {
+    if (window.location.protocol === "https:") {
       isSecureCookie = true;
     }
-  } catch (e: any) {
-    if (isRscHeadersUnavailableError(e)) {
-      // Ignore
-    } else {
-      throw e;
+    Cookies.set(name, value, {
+      secure: isSecureCookie,
+      expires: options.maxAge === undefined ? undefined : new Date(Date.now() + (options.maxAge) * 1000),
+      sameSite: "Strict"
+    });
+  } else {
+    if (rscHeaders().get("x-forwarded-proto") === "https") {
+      isSecureCookie = true;
     }
-  }
-  // ================================
 
-  try {
     rscCookies().set(name, value, {
       secure: isSecureCookie,
       maxAge: options.maxAge,
     });
-  } catch (e: any) {
-    if (isRscCookieUnavailableError(e)) {
-      Cookies.set(name, value, {
-        secure: isSecureCookie,
-        expires: options.maxAge === undefined ? undefined : new Date(Date.now() + (options.maxAge) * 1000),
-        sameSite: "Strict"
-      });
-    } else {
-      throw e;
-    }
   }
 }
 
