f23567d90e11cc3597f516caefc687c81ec477cd
Zai Shi <zaishi00@outlook.com>
2024-10-15 10:51:51 -0700

    added user-button image

--- examples/docs-examples/src/app/components/user-button/page.tsx
+++ examples/docs-examples/src/app/components/user-button/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
     <div style={{ marginLeft: '30px'}}>
       <h1>User Button</h1>
       <UserButton
-        showUserInfo={true}
         colorModeToggle={() => { console.log("color mode toggle clicked"); }}
       />
     </div>
