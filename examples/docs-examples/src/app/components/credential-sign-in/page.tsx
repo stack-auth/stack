17755dc8dc20b2fbedec5ff36e872072bcef8ec8
Zai Shi <zaishi00@outlook.com>
2024-10-14 20:14:26 -0700

    added credential sign in component image

---
+++ examples/docs-examples/src/app/components/credential-sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { CredentialSignIn } from '@stackframe/stack';
+
+export default function CredentialSignInPage() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <CredentialSignIn />
+    </div>
+  );
+}
