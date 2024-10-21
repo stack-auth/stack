810bfb20ca66bce5eaf7f07012e4030c75222057
Zai Shi <zaishi00@outlook.com>
2024-10-21 22:08:09 +0200

    Update packages/stack-shared/src/schema-fields.ts
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>

--- packages/stack-shared/src/schema-fields.ts
+++ packages/stack-shared/src/schema-fields.ts
@@ -271,7 +271,7 @@ export const userLastActiveAtMillisSchema = yupNumber().nullable().meta({ openap
 export const userOtpAuthEnabledSchema = yupBoolean().meta({ openapiField: { hidden: true, description: 'Whether the user has OTP/magic link enabled. ', exampleValue: true } });
 export const userOtpAuthEnabledMutationSchema = yupBoolean().meta({ openapiField: { hidden: true, description: 'Whether the user has OTP/magic link enabled. Note that only accounts with verified emails can sign-in with OTP.', exampleValue: true } });
 export const userHasPasswordSchema = yupBoolean().meta({ openapiField: { hidden: true, description: 'Whether the user has a password set. If the user does not have a password set, it will not be able to sign in with email/password.', exampleValue: true } });
-export const userPasswordSchema = yupString().meta({ openapiField: { description: 'A new password for the user, overwriting the old one (if it exists). Specifying this option revokes all current sessions.', exampleValue: 'my-new-password' } });
+export const userPasswordMutationSchema = yupString().meta({ openapiField: { description: 'Sets the user's password. Doing so revokes all current sessions.', exampleValue: 'my-new-password' } });
 export const userTotpSecretSchema = base64Schema.meta({ openapiField: { description: 'A TOTP secret for the user, overwriting the old one (if it exists). Set to null to disable 2FA.', exampleValue: 'dG90cC1zZWNyZXQ=' } });
 
 // Auth
