27190957cdd8d61e65e3c1ecf6aa9ff4772781b8
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-10-24 17:47:25 -0700

    works better

--- examples/demo/src/app/handler/[...stack]/page.tsx
+++ examples/demo/src/app/handler/[...stack]/page.tsx
@@ -3,7 +3,7 @@ import { stackServerApp } from "src/stack";
 
 export default function Handler(props) {
   return (
-    <StackHandler fullPage app={stackServerApp} /* @next-codemod-error 'props' is used with spread syntax (...). Any asynchronous properties of 'props' must be awaited when accessed. */
-    {...props} />
+    <StackHandler fullPage app={stackServerApp} /* @nearror 'props' is used with spread syntax (...). Any asynchronous properties of 'props' must be awaited when accessed. */
+      {...props} />
   );
 }
