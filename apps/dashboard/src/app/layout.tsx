9861c00a3e483266ea4565df7ec4b8dd82ae5844
Zai Shi <zaishi00@outlook.com>
2024-11-01 13:17:41 -0700

    fixed NEXT_PUBLIC_STACK_HEAD_TAGS

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode,
 }) {
-  const headTags: TagConfigJson[] = JSON.parse(getEnvVariable('NEXT_PUBLIC_STACK_HEAD_TAGS'));
+  const headTags: TagConfigJson[] = JSON.parse(getEnvVariable('NEXT_PUBLIC_STACK_HEAD_TAGS', '[]'));
   const translationLocale = getEnvVariable('STACK_DEVELOPMENT_TRANSLATION_LOCALE', "") || undefined;
   if (translationLocale !== undefined && getNodeEnvironment() !== 'development') {
     throw new Error(`STACK_DEVELOPMENT_TRANSLATION_LOCALE can only be used in development mode (found: ${JSON.stringify(translationLocale)})`);
