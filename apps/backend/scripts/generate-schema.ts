6ae7af6cb6351d0bd3409792503d77aca32a1f3f
Zai Shi <zaishi00@outlook.com>
2024-12-07 11:04:31 -0800

    migration

--- apps/backend/scripts/generate-schema.ts
+++ apps/backend/scripts/generate-schema.ts
@@ -17,12 +17,17 @@ async function main() {
       }
 
       for (const overload of handler.overloads.values()) {
+        let audienceCount = 0;
         for (const audience of ['client', 'server', 'admin'] as const) {
           const schemaAudience = overload.request.describe().fields.auth?.fields?.type;
           if (!schemaAudience) continue;
           if ("oneOf" in schemaAudience && schemaAudience.oneOf.length > 0 && schemaAudience.oneOf.includes(audience)) {
-            console.log(`    ${audience}`);
+            audienceCount++;
+            console.log(`      ${audience}`);
           }
+        }
+        if (handler.overloads.size !== 1 && audienceCount !== handler.overloads.size) {
+          throw new Error(`Expected ${handler.overloads.size} audiences, got ${audienceCount}`);
         }
       }
     });
