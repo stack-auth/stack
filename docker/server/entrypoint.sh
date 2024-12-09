55a6309277f478df7ba3c863a3dd06e8e7484ae6
Zai Shi <zaishi00@outlook.com>
2024-12-08 16:29:05 -0800

    updated self host vars, reduced the number of required env vars

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -2,25 +2,29 @@
 
 set -e
 
+export STACK_SEED_INTERNAL_PROJECT_PUBLISHABLE_CLIENT_KEY=$(openssl rand -base64 32)
+export STACK_SEED_INTERNAL_PROJECT_SECRET_SERVER_KEY=$(openssl rand -base64 32)
+export STACK_SEED_INTERNAL_PROJECT_SUPER_SECRET_ADMIN_KEY=$(openssl rand -base64 32)
+
 export NEXT_PUBLIC_STACK_PROJECT_ID=internal
 export NEXT_PUBLIC_STACK_PUBLISHABLE_CLIENT_KEY=${STACK_SEED_INTERNAL_PROJECT_PUBLISHABLE_CLIENT_KEY}
 export STACK_SECRET_SERVER_KEY=${STACK_SEED_INTERNAL_PROJECT_SECRET_SERVER_KEY}
 export STACK_SUPER_SECRET_ADMIN_KEY=${STACK_SEED_INTERNAL_PROJECT_SUPER_SECRET_ADMIN_KEY}
 
-if [ "$STACK_RUN_MIGRATIONS" = "true" ]; then
+if [ "$STACK_SKIP_MIGRATIONS" = "true" ]; then
+  echo "Skipping migrations."
+else
   echo "Running migrations..."
   prisma migrate deploy --schema=./apps/backend/prisma/schema.prisma
-else
-  echo "Skipping migrations."
 fi
 
-if [ "$STACK_RUN_SEED_SCRIPT" = "true" ]; then
+if [ "$STACK_SKIP_SEED_SCRIPT" = "true" ]; then
+  echo "Skipping seed script."
+else
   echo "Running seed script..."
   cd apps/backend
   node seed.js
   cd ../..
-else
-  echo "Skipping seed script."
 fi
 
 # Start backend and dashboard in parallel
