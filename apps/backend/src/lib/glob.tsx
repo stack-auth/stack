76d399f06a7f43ba8858dc3a32d96b3a63a72e39
Zai Shi <zaishi00@outlook.com>
2024-12-09 15:21:12 -0800

    fixed migration

--- apps/backend/src/lib/glob.tsx
+++ apps/backend/src/lib/glob.tsx
@@ -1,5 +1,6 @@
 import { isSmartRouteHandler } from '@/route-handlers/smart-route-handler';
 import { HTTP_METHODS } from '@stackframe/stack-shared/dist/utils/http';
+import { typedKeys } from '@stackframe/stack-shared/dist/utils/objects';
 import { glob } from 'glob';
 import path from 'path';
 
@@ -8,22 +9,21 @@ export async function listEndpoints(basePath: string) {
   const importPathPrefix = `@/app/${basePath}`;
   const filePaths = [...await glob(filePathPrefix + "/**/route.{js,jsx,ts,tsx}")];
 
-  const endpoints = new Map(
-    filePaths.map((filePath) => {
-      if (!filePath.startsWith(filePathPrefix)) {
-        throw new Error(`Invalid file path: ${filePath}`);
-      }
-      const suffix = filePath.slice(filePathPrefix.length);
-      const url = suffix.slice(0, suffix.lastIndexOf("/route."));
-      const importPath = `${importPathPrefix}${suffix}`;
-      const myModule = require(importPath);
-      const handlersByMethod = new Map(
-        HTTP_METHODS.map(method => [method, myModule[method]] as const)
-          .filter(([_, handler]) => isSmartRouteHandler(handler))
-      );
-      return [url, handlersByMethod] as const;
-    }).filter((x) => x[1].size > 0)
-  );
+  const endpoints = new Map(await Promise.all(filePaths.map(async (filePath) => {
+    if (!filePath.startsWith(filePathPrefix)) {
+      throw new Error(`Invalid file path: ${filePath}`);
+    }
+    const suffix = filePath.slice(filePathPrefix.length);
+    const midfix = suffix.slice(0, suffix.lastIndexOf("/route."));
+    const importPath = `${importPathPrefix}${suffix}`;
+    const urlPath = midfix.replaceAll("[", "{").replaceAll("]", "}");
+    const myModule = require(importPath);
+    const handlersByMethod = new Map(
+      typedKeys(HTTP_METHODS).map(method => [method, myModule[method]] as const)
+        .filter(([_, handler]) => isSmartRouteHandler(handler))
+    );
+    return [urlPath, handlersByMethod] as const;
+  })));
 
   return endpoints;
 }
