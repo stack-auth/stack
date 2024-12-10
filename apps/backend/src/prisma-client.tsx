c28aec0ffa05a7b97455b6e24a80d659c6ca3902
Zai Shi <zaishi00@outlook.com>
2024-12-10 12:39:50 -0800

    not use edge client

--- apps/backend/src/prisma-client.tsx
+++ apps/backend/src/prisma-client.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { getNodeEnvironment } from '@stackframe/stack-shared/dist/utils/env';
 
