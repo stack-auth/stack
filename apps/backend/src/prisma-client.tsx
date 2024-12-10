f30de21cfcea484965abc86f612eb90973697f6f
Zai Shi <zaishi00@outlook.com>
2024-12-10 22:07:39 +0100

    Added Prisma Accelerate (#363)

--- apps/backend/src/prisma-client.tsx
+++ apps/backend/src/prisma-client.tsx
@@ -1,12 +1,15 @@
-import { PrismaClient } from '@prisma/client';
-import { getNodeEnvironment } from '@stackframe/stack-shared/dist/utils/env';
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { getEnvVariable, getNodeEnvironment } from '@stackframe/stack-shared/dist/utils/env';
 
 // In dev mode, fast refresh causes us to recreate many Prisma clients, eventually overloading the database.
 // Therefore, only create one Prisma client in dev mode.
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const useAccelerate = getEnvVariable('STACK_ACCELERATE_ENABLED', 'false') === 'true';
+
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-export const prismaClient = globalForPrisma.prisma || new PrismaClient();
+export const prismaClient = globalForPrisma.prisma || (useAccelerate ? new PrismaClient().$extends(withAccelerate()) : new PrismaClient());
 
 if (getNodeEnvironment() !== 'production') {
   globalForPrisma.prisma = prismaClient;
