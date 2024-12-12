f145e597c0fab648059e21a22c30480c4a3f0e27
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-12-11 18:56:42 -0800

    Increase Sentry maxValueLength

--- apps/dashboard/sentry.client.config.ts
+++ apps/dashboard/sentry.client.config.ts
@@ -4,30 +4,15 @@
 
 import * as Sentry from "@sentry/nextjs";
 import { getBrowserCompatibilityProblems } from "@stackframe/stack-shared/dist/utils/browser-compat";
+import { sentryBaseConfig } from "@stackframe/stack-shared/dist/utils/sentry";
 import { nicify } from "@stackframe/stack-shared/dist/utils/strings";
 
 Sentry.init({
+  ...sentryBaseConfig,
+
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  ignoreErrors: [
-		// React throws these errors when used with some browser extensions (eg. Google Translate)
-		"NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.",
-		"NotFoundError: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.",
-	],
-
-  normalizeDepth: 5,
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
   enabled: process.env.NODE_ENV !== "development" && !process.env.CI,
-
-  replaysOnErrorSampleRate: 1.0,
-
-  replaysSessionSampleRate: 1.0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
