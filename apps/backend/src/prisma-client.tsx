23d496b99aa1dd02543c4f2171edc08a8eaa34ed
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-12-12 14:01:08 -0800

    Fail instead of retrying Prisma tx without transaction

--- apps/backend/src/prisma-client.tsx
+++ apps/backend/src/prisma-client.tsx
@@ -1,7 +1,6 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { getEnvVariable, getNodeEnvironment } from '@stackframe/stack-shared/dist/utils/env';
-import { StackAssertionError, captureError } from "@stackframe/stack-shared/dist/utils/errors";
 import { Result } from "@stackframe/stack-shared/dist/utils/results";
 
 // In dev mode, fast refresh causes us to recreate many Prisma clients, eventually overloading the database.
@@ -33,15 +32,5 @@ export async function maybeTransactionWithRetry<T>(fn: (...args: Parameters<Para
     }
   }, isDev ? 1 : 3);
 
-  if (res.status === 'ok') {
-    return res.data;
-  }
-
-  const retriesFailedWarning = new StackAssertionError("Failed to execute transaction despite retries! Falling back to non-transactional execution, which may cause data inconsistency.", { cause: res.error });
-  if (isDev) {
-    throw retriesFailedWarning;
-  } else {
-    captureError('maybeTransactionWithRetry', retriesFailedWarning);
-    return await fn(prismaClient);
-  }
+  return Result.orThrow(res);
 }
