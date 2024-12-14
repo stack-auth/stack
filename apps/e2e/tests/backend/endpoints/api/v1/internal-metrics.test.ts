f96264f6c5f05c9c69259be402ed6123fbaf7345
TheCactusBlue <thecactusblue@gmail.com>
2024-12-14 20:07:45 +0000

    another test case

--- apps/e2e/tests/backend/endpoints/api/v1/internal-metrics.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/internal-metrics.test.ts
@@ -429,4 +429,33 @@ describe("/internal/metrics route", () => {
       }
     `);
   });
+
+  it("should not work for non-admins", async ({ expect }) => {
+    await Project.createAndSwitch({
+      config: {
+        magic_link_enabled: true,
+      }
+    });
+
+    await Auth.Otp.signIn();
+
+    const response = await niceBackendFetch("/api/v1/internal/metrics", { accessType: 'client' });
+    expect(response).toMatchInlineSnapshot(`
+      NiceResponse {
+        "status": 401,
+        "body": {
+          "code": "INSUFFICIENT_ACCESS_TYPE",
+          "details": {
+            "actual_access_type": "client",
+            "allowed_access_types": ["admin"],
+          },
+          "error": "The x-stack-access-type header must be 'admin', but was 'client'.",
+        },
+        "headers": Headers {
+          "x-stack-known-error": "INSUFFICIENT_ACCESS_TYPE",
+          <some fields may have been hidden>,
+        },
+      }
+    `);
+  });
 });
