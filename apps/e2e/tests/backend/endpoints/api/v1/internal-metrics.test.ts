de0c79ac50a945641c58d39fd700d00cc0236654
CactusBlue <thecactusblue@gmail.com>
2024-12-14 11:49:52 -0800

    Increase the number of loops
    Co-authored-by: Konsti Wohlwend <n2d4xc@gmail.com>

--- apps/e2e/tests/backend/endpoints/api/v1/internal-metrics.test.ts
+++ apps/e2e/tests/backend/endpoints/api/v1/internal-metrics.test.ts
@@ -10,8 +10,7 @@ describe("/internal/metrics route", () => {
       }
     });
 
-    // loop 10 times
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 15; i++) {
       backendContext.set({ mailbox: createMailbox() });
       await Auth.Otp.signIn();
     }
