3aa483c5051987172b8a9488d1fbace97ec9a22d
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-12-19 23:11:46 -0800

    Improve globe

--- apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/metrics/globe.tsx
+++ apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/metrics/globe.tsx
@@ -1,9 +1,10 @@
 import useResizeObserver from '@react-hook/resize-observer';
-import { Card, CardContent, CardDescription, CardTitle } from '@stackframe/stack-ui';
-import { RefObject, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { throwErr } from '@stackframe/stack-shared/dist/utils/errors';
+import { Card, CardContent, CardDescription, CardTitle, Skeleton } from '@stackframe/stack-ui';
+import { RefObject, use, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Globe, { GlobeMethods } from 'react-globe.gl';
 
-import countries from './country-data.geo.json';
+const countriesPromise = import('./country-data.geo.json');
 
 function useSize(target: RefObject<HTMLDivElement>) {
   const [size, setSize] = useState<DOMRectReadOnly>();
@@ -19,61 +20,147 @@ function useSize(target: RefObject<HTMLDivElement>) {
 }
 
 export function GlobeSection({ countryData, children }: {countryData: Record<string, number>, children?: React.ReactNode}) {
+  const countries = use(countriesPromise);
   const globeRef = useRef<GlobeMethods>();
   const ref = useRef<HTMLDivElement>(null);
   const size = useSize(ref);
 
-  const [selectedCountry, setSelectedCountry] = useState<{ code: string, name: string } | null>(null);
+  const [hexSelectedCountry, setHexSelectedCountry] = useState<{ code: string, name: string } | null>(null);
+  const [polygonSelectedCountry, setPolygonSelectedCountry] = useState<{ code: string, name: string } | null>(null);
+  const selectedCountry = hexSelectedCountry ?? polygonSelectedCountry ?? null;
 
-  const maxUserRatio = useMemo(
-    () => {
-      return Math.max(...countries.features.map(x =>
-        (countryData[x.properties.ISO_A2] ?? 0) / (x.properties.POP_EST)
-      ), 0);
-    },
-    [countryData]
-  );
+  const [isGlobeReady, setIsGlobeReady] = useState(false);
 
+  const [isLightMode, setIsLightMode] = useState<boolean | null>(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    if (globeRef.current) {
-      globeRef.current.pointOfView({ altitude: 1 });
-    }
-  }, [globeRef]);
+    const updateIsLightMode = () => {
+      const shouldBeLight = getComputedStyle(document.documentElement).getPropertyValue('color-scheme') === 'light';
+      if (shouldBeLight !== isLightMode) {
+        setIsLightMode(shouldBeLight);
+      }
+    };
+    updateIsLightMode();
+    const interval = setInterval(updateIsLightMode, 1000);
+    return () => clearInterval(interval);
+  }, [isLightMode]);
+
+  // calculate color values for each country
+  const totalUsers = Object.values(countryData).reduce((acc, curr) => acc + curr, 0);
+  const totalPopulation = countries.features.reduce((acc, curr) => acc + curr.properties.POP_EST, 0);
+  const colorValues = new Map(countries.features.map((country) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    const countryUsers = countryData[country.properties.ISO_A2_EH] ?? 0;
+    const countryPopulation = country.properties.POP_EST;
+    if (countryUsers === 0) return [country.properties.ISO_A2_EH, null] as const;
+
+    // we want to get the lowest proportion such that there's a 95% chance that it's higher than the actual
+    // proportion (given enough samples)
+    // my math sucks, someone please correct me if I'm wrong (but the colors look nice)
+    const observedProportion = countryUsers / totalUsers;
+    const standardError = Math.sqrt(observedProportion * (1 - observedProportion) / totalUsers);
+    const zScore = 1.645; // one-sided 95% confidence interval
+
+    const proportionLowerBound = Math.max(0, observedProportion - zScore * standardError);  // how likely is it that a random user is in this country? (with 95% confidence lower bound from above)
+    const populationProportion = countryPopulation / totalPopulation;  // how likely is it that a random person is in this country?
+    const likelihoodRatio = proportionLowerBound / populationProportion;  // how much more likely is it for a random user to be in this country than a random person?
+
+    const colorValue = Math.log(Math.max(1, 100 * likelihoodRatio));
+
+    return [country.properties.ISO_A2_EH, colorValue] as const;
+  }));
+  const maxColorValue = Math.max(0, ...[...colorValues.values()].filter((v): v is number => v !== null));
 
 
   return <div className='flex w-full gap-4 flex-col xl:flex-row'>
-    <div ref={ref} className='w-full xl:w-8/12 rounded-lg h-[300px] xl:h-[500px] border border-1 border-border overflow-hidden'>
-      <Globe
+    <div ref={ref} className='relative w-full xl:w-8/12 rounded-lg h-[300px] xl:h-[500px] border border-1 border-border overflow-hidden'>
+      {!isGlobeReady && (
+        <Skeleton style={{
+          width: Math.min(size?.width ?? 0, size?.height ?? 0) * 1.9 / 3,
+          height: Math.min(size?.width ?? 0, size?.height ?? 0) * 1.9 / 3,
+          borderRadius: '100%',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }} />
+      )}
+      {isLightMode !== null && <Globe
+        key={isLightMode ? 'light' : 'dark'}
         ref={globeRef}
-        globeImageUrl='/static/globe_background.png'
+        backgroundColor='#00000000'
+        globeImageUrl={
+          isLightMode
+            ? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAAaADAAQAAAABAAAAAQAAAAD5Ip3+AAAADUlEQVQIHWO48vjffwAI+QO1AqIWWgAAAABJRU5ErkJggg=='
+            : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAAaADAAQAAAABAAAAAQAAAAD5Ip3+AAAADUlEQVQIHWPgF9f8DwAB1wFPLWQXmAAAAABJRU5ErkJggg=='
+        }
         width={size?.width ?? 0}
         height={size?.height ?? 0}
+        onGlobeReady={() => {
+          setTimeout(() => setIsGlobeReady(true), 100);
+          const current = globeRef.current ?? throwErr('Globe ref not available even though globe is ready?');
+          const controls = current.controls();
+          controls.maxDistance = 1000;
+          controls.minDistance = 120;
+          controls.dampingFactor = 0.3;
+          console.log('controls', controls);
+        }}
+        animateIn={false}
+
+        polygonsData={countries.features}
+        polygonCapColor={() => "transparent"}
+        polygonSideColor={() => "transparent"}
+        polygonAltitude={0.002}
+        onPolygonHover={(d: any) => {
+          if (d) {
+            setPolygonSelectedCountry({ code: d.properties.ISO_A2_EH, name: d.properties.NAME });
+          } else {
+            setPolygonSelectedCountry(null);
+          }
+        }}
+
         hexPolygonsData={countries.features}
         hexPolygonResolution={3}
         hexPolygonMargin={0.2}
-        hexPolygonAltitude={0.02}
-        hexPolygonColor={(d: any) => {
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-          const userRatio = (countryData[d.properties.ISO_A2] ?? 0) / d.properties.POP_EST;
-          // 0 for smallest, 1 for biggest
-          const clamped = userRatio / maxUserRatio;
+        hexPolygonAltitude={0.003}
+        hexPolygonColor={(country: any) => {
+          const createColor = (value: number | null) => {
+            // Chrome's WebGL is pretty fast so we can afford to do on-hover highlights
+            const highlight = "chrome" in window && country.properties.ISO_A2_EH === selectedCountry?.code;
 
-          return `hsl(271, 84%, ${20 + 40 * (isNaN(clamped) ? 0 : clamped) + 4}%)`;
+            if (Number.isNaN(value) || value === null || maxColorValue < 0.0001) {
+              if (isLightMode) {
+                return `hsl(210, 17.20%, ${highlight ? '55.5%' : '45.5%'})`;
+              } else {
+                if (value === null && maxColorValue < 0.0001) {
+                  // if there are no users at all, in dark mode, show the globe in a slightly lighter color
+                  return `hsl(271, 84%, ${highlight ? '30%' : '20%'})`;
+                } else {
+                  return `hsl(271, 84%, ${highlight ? '25%' : '15%'})`;
+                }
+              }
+            }
+            const scaled = value / maxColorValue;
+            if (isLightMode) {
+              return `hsl(${175 * (1 - scaled)}, 100%, ${20 + 70 * scaled + (highlight ? 10 : 0)}%)`;
+            } else {
+              return `hsl(271, 84%, ${24 + 40 * scaled + (highlight ? 10 : 0)}%)`;
+            }
+          };
+          const color = createColor(colorValues.get(country.properties.ISO_A2_EH) ?? null);
+          return color;
         }}
         onHexPolygonHover={(d: any) => {
           if (d) {
-            setSelectedCountry({ code: d.properties.ISO_A2, name: d.properties.NAME });
+            setHexSelectedCountry({ code: d.properties.ISO_A2_EH, name: d.properties.NAME });
+          } else {
+            setHexSelectedCountry(null);
           }
         }}
 
         atmosphereColor='#CBD5E0'
         atmosphereAltitude={0.2}
-
-        // ringsData={ringData}
-        ringMaxRadius={3}
-        ringRepeatPeriod={1000}
-        ringColor={() => "#7CACFF"}
-      />
+      />}
     </div>
     <div className='h-full w-full xl:w-4/12 flex flex-col gap-4'>
       {children}
