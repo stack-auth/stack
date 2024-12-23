e200a87800427965b43478d49d3a9922a00fc6b2
Zai Shi <zaishi00@outlook.com>
2024-12-23 14:57:40 -0800

    removed traces sample rate

--- apps/backend/sentry.client.config.ts
+++ apps/backend/sentry.client.config.ts
@@ -2,47 +2,51 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// import * as Sentry from "@sentry/nextjs";
-// import { getBrowserCompatibilityReport } from "@stackframe/stack-shared/dist/utils/browser-compat";
-// import { sentryBaseConfig } from "@stackframe/stack-shared/dist/utils/sentry";
-// import { nicify } from "@stackframe/stack-shared/dist/utils/strings";
+import * as Sentry from "@sentry/nextjs";
+import { getBrowserCompatibilityReport } from "@stackframe/stack-shared/dist/utils/browser-compat";
+import { sentryBaseConfig } from "@stackframe/stack-shared/dist/utils/sentry";
+import { nicify } from "@stackframe/stack-shared/dist/utils/strings";
 
-// Sentry.init({
-//   ...sentryBaseConfig,
+Sentry.init({
+  ...sentryBaseConfig,
 
-//   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-//   enabled: process.env.NODE_ENV !== "development" && !process.env.CI,
+  enabled: process.env.NODE_ENV !== "development" && !process.env.CI,
 
-//   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-//   integrations: [
-//     Sentry.replayIntegration({
-//       // Additional Replay configuration goes in here, for example:
-//       maskAllText: false,
-//       blockAllMedia: false,
-//     }),
-//   ],
+  // Sentry and Prisma are not compatible with the new tracing. https://github.com/prisma/prisma/issues/25885
+  // TODO: Remove this once sentry fixes the issue.
+  tracesSampleRate: 0,
 
-//   // Add exception metadata to the event
-//   beforeSend(event, hint) {
-//     const error = hint.originalException;
-//     let nicified;
-//     try {
-//       nicified = nicify(error, { maxDepth: 8 });
-//     } catch (e) {
-//       nicified = `Error occurred during nicification: ${e}`;
-//     }
-//     if (error instanceof Error) {
-//       event.extra = {
-//         ...event.extra,
-//         cause: error.cause,
-//         errorProps: {
-//           ...error,
-//         },
-//         nicifiedError: nicified,
-//         clientBrowserCompatibility: getBrowserCompatibilityReport(),
-//       };
-//     }
-//     return event;
-//   },
-// });
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+
+  // Add exception metadata to the event
+  beforeSend(event, hint) {
+    const error = hint.originalException;
+    let nicified;
+    try {
+      nicified = nicify(error, { maxDepth: 8 });
+    } catch (e) {
+      nicified = `Error occurred during nicification: ${e}`;
+    }
+    if (error instanceof Error) {
+      event.extra = {
+        ...event.extra,
+        cause: error.cause,
+        errorProps: {
+          ...error,
+        },
+        nicifiedError: nicified,
+        clientBrowserCompatibility: getBrowserCompatibilityReport(),
+      };
+    }
+    return event;
+  },
+});
