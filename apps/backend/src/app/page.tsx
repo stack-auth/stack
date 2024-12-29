2b09cfb841b7aae05327aaf0d16f8f459ffc6853
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-12-29 11:34:14 -0800

    Fix indentation

--- apps/backend/src/app/page.tsx
+++ apps/backend/src/app/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-    Welcome to Stack Auth&apos;s API endpoint.<br />
+      Welcome to Stack Auth&apos;s API endpoint.<br />
       <br />
-    Were you looking for <Link href="https://app.stack-auth.com">Stack&apos;s dashboard</Link> instead?<br />
+      Were you looking for <Link href="https://app.stack-auth.com">Stack&apos;s dashboard</Link> instead?<br />
       <br />
       You can also return to <Link href="https://stack-auth.com">https://stack-auth.com</Link>.<br />
       <br />
