f11bd9ec49dd32877d8f8edc70ab3eec435815cf
Konstantin Wohlwend <n2d4xc@gmail.com>
2024-12-29 20:22:34 -0800

    Make onboarding dialog non-blocking

--- apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/layout.tsx
+++ apps/dashboard/src/app/(main)/(protected)/projects/[projectId]/layout.tsx
@@ -1,11 +1,15 @@
-import { AdminAppProvider } from "./use-admin-app";
+import { Suspense } from "react";
 import { OnboardingDialog } from "./onboarding-dialog";
 import SidebarLayout from "./sidebar-layout";
+import { AdminAppProvider } from "./use-admin-app";
 
 export default function Layout(props: { children: React.ReactNode, params: { projectId: string } }) {
   return (
     <AdminAppProvider projectId={props.params.projectId}>
-      <OnboardingDialog />
+      {/* Don't block the rest of the page for the dialog, so wrap it with a Suspense */}
+      <Suspense fallback={<></>}>
+        <OnboardingDialog />
+      </Suspense>
       <SidebarLayout projectId={props.params.projectId}>
         {props.children}
       </SidebarLayout>
