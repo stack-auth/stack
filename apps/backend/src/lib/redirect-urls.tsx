4e03a16f4bd3dc512a9b1156a42bfdb0d982354c
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-01-13 10:51:56 -0800

    Fix STACK-BACKEND-63

--- apps/backend/src/lib/redirect-urls.tsx
+++ apps/backend/src/lib/redirect-urls.tsx
@@ -1,13 +1,21 @@
-import { isLocalhost } from "@stackframe/stack-shared/dist/utils/urls";
+import { StackAssertionError, captureError } from "@stackframe/stack-shared/dist/utils/errors";
+import { createUrlIfValid, isLocalhost } from "@stackframe/stack-shared/dist/utils/urls";
 
 export function validateRedirectUrl(urlOrString: string | URL, domains: { domain: string, handler_path: string }[], allowLocalhost: boolean): boolean {
-  const url = new URL(urlOrString);
+  const url = createUrlIfValid(urlOrString);
+  if (!url) return false;
   if (allowLocalhost && isLocalhost(url)) {
     return true;
   }
   return domains.some((domain) => {
     const testUrl = url;
-    const baseUrl = new URL(domain.domain);
+    const baseUrl = createUrlIfValid(domain.domain);
+    if (!baseUrl) {
+      captureError("invalid-redirect-domain", new StackAssertionError("Invalid redirect domain; maybe this should be fixed in the database", {
+        domain: domain.domain,
+      }));
+      return false;
+    }
 
     const sameOrigin = baseUrl.protocol === testUrl.protocol && baseUrl.hostname === testUrl.hostname;
     const isSubPath = testUrl.pathname.startsWith(baseUrl.pathname);
