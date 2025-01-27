a6b4398f046b1ee6dc6959fe3855627b91511086
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-01-27 14:32:28 -0800

    getTokens now refreshes expired tokens

--- packages/stack-shared/src/sessions.ts
+++ packages/stack-shared/src/sessions.ts
@@ -1,3 +1,4 @@
+import * as jose from 'jose';
 import { StackAssertionError } from "./utils/errors";
 import { Store } from "./utils/stores";
 
@@ -8,6 +9,23 @@ export class AccessToken {
     if (token === "undefined") {
       throw new StackAssertionError("Access token is the string 'undefined'; it's unlikely this is the correct value. They're supposed to be unguessable!");
     }
+  }
+
+  get expiresAt(): Date {
+    const { exp } = jose.decodeJwt(this.token);
+    if (!exp) return new Date(8640000000000000);  // max date value
+    return new Date(exp * 1000);
+  }
+
+  /**
+   * @returns The number of milliseconds until the access token expires, or 0 if it has already expired.
+   */
+  get expiresInMillis(): number {
+    return Math.max(0, this.expiresAt.getTime() - Date.now());
+  }
+
+  isExpired(): boolean {
+    return this.expiresInMillis <= 0;
   }
 }
 
@@ -97,13 +115,25 @@ export class InternalSession {
   /**
    * Returns the access token if it is found in the cache, fetching it otherwise.
    *
-   * This is usually the function you want to call to get an access token. When using the access token, you should catch errors that occur if it expires, and call `markAccessTokenExpired` to mark the token as expired if so (after which a call to this function will always refetch the token).
+   * This is usually the function you want to call to get an access token. Either set `minMillisUntilExpiration` to a reasonable value, or catch errors that occur if it expires, and call `markAccessTokenExpired` to mark the token as expired if so (after which a call to this function will always refetch the token).
    *
    * @returns null if the session is known to be invalid, cached tokens if they exist in the cache (which may or may not be valid still), or new tokens otherwise.
    */
-  async getPotentiallyExpiredTokens(): Promise<{ accessToken: AccessToken, refreshToken: RefreshToken | null } | null> {
-    const accessToken = await this._getPotentiallyExpiredAccessToken();
-    return accessToken ? { accessToken, refreshToken: this._refreshToken } : null;
+  async getOrFetchLikelyValidTokens(minMillisUntilExpiration: number): Promise<{ accessToken: AccessToken, refreshToken: RefreshToken | null } | null> {
+    if (minMillisUntilExpiration >= 60_000) {
+      throw new Error(`Required access token expiry ${minMillisUntilExpiration}ms is too long; access tokens are too short to be used for more than 60s`);
+    }
+
+    const accessToken = this._getPotentiallyInvalidAccessTokenIfAvailable();
+    if (!accessToken || accessToken.expiresInMillis < minMillisUntilExpiration) {
+      const newTokens = await this.fetchNewTokens();
+      const expiresInMillis = newTokens?.accessToken.expiresInMillis;
+      if (expiresInMillis && expiresInMillis < minMillisUntilExpiration) {
+        throw new StackAssertionError(`Required access token expiry ${minMillisUntilExpiration}ms is too long; access tokens are too short when they're generated (${expiresInMillis}ms)`);
+      }
+      return newTokens;
+    }
+    return { accessToken, refreshToken: this._refreshToken };
   }
 
   /**
@@ -111,7 +141,7 @@ export class InternalSession {
    *
    * The newly generated tokens are shortlived, so it's good practice not to rely on their validity (if possible). However, this function is useful in some cases where you only want to pass access tokens to a service, and you want to make sure said access token has the longest possible lifetime.
    *
-   * In most cases, you should prefer `getPotentiallyExpiredTokens` with a fallback to `markAccessTokenExpired` and a retry mechanism if the endpoint rejects the token.
+   * In most cases, you should prefer `getOrFetchLikelyValidTokens` with a fallback to `markAccessTokenExpired` and a retry mechanism if the endpoint rejects the token.
    *
    * @returns null if the session is known to be invalid, or new tokens otherwise (which, at the time of fetching, are guaranteed to be valid).
    */
@@ -121,6 +151,7 @@ export class InternalSession {
   }
 
   markAccessTokenExpired(accessToken: AccessToken) {
+    // TODO we don't need this anymore, since we now check the expiry by ourselves
     if (this._accessToken.get() === accessToken) {
       this._accessToken.set(null);
     }
@@ -134,12 +165,21 @@ export class InternalSession {
   }
 
   /**
+   * @returns An access token, which may be expired or expire soon, or null if it is known to be invalid.
+   */
+  private _getPotentiallyInvalidAccessTokenIfAvailable(): AccessToken | null {
+    const accessToken = this._accessToken.get();
+    if (accessToken && !accessToken.isExpired()) return accessToken;
+    return null;
+  }
+
+  /**
    * @returns An access token (cached if possible), or null if the session either does not represent a user or the session is invalid.
    */
-  private async _getPotentiallyExpiredAccessToken(): Promise<AccessToken | null> {
+  private async _getOrFetchPotentiallyInvalidAccessToken(): Promise<AccessToken | null> {
     if (!this._refreshToken) return null;
-    if (this._knownToBeInvalid.get()) return null;
-    const oldAccessToken = this._accessToken.get();
+    if (this.isKnownToBeInvalid()) return null;
+    const oldAccessToken = this._getPotentiallyInvalidAccessTokenIfAvailable();
     if (oldAccessToken) return oldAccessToken;
 
     // refresh access token
@@ -150,7 +190,7 @@ export class InternalSession {
   }
 
   /**
-   * You should prefer `getPotentiallyExpiredAccessToken` in almost all cases.
+   * You should prefer `_getOrFetchAccessToken` in almost all cases.
    *
    * @returns A newly fetched access token (never read from cache), or null if the session either does not represent a user or the session is invalid.
    */
