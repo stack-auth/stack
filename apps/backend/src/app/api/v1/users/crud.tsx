e27f7746c2643e1d50114df8bc81b4712c6fa9b2
CactusBlue <thecactusblue@gmail.com>
2025-01-31 10:54:16 -0800

    User ID Search, Again (#414)

--- apps/backend/src/app/api/v1/users/crud.tsx
+++ apps/backend/src/app/api/v1/users/crud.tsx
@@ -17,6 +17,7 @@ import { hashPassword, isPasswordHashValid } from "@stackframe/stack-shared/dist
 import { deepPlainEquals } from "@stackframe/stack-shared/dist/utils/objects";
 import { createLazyProxy } from "@stackframe/stack-shared/dist/utils/proxies";
 import { typedToLowercase } from "@stackframe/stack-shared/dist/utils/strings";
+import { isUuid } from "@stackframe/stack-shared/dist/utils/uuids";
 import { teamPrismaToCrud, teamsCrudHandlers } from "../teams/crud";
 
 export const userFullInclude = {
@@ -444,7 +445,7 @@ export const usersCrudHandlers = createLazyProxy(() => createCrudHandlers(usersC
     cursor: yupString().uuid().optional().meta({ openapiField: { onlyShowInOperations: [ 'List' ], description: "The cursor to start the result set from." } }),
     order_by: yupString().oneOf(['signed_up_at']).optional().meta({ openapiField: { onlyShowInOperations: [ 'List' ], description: "The field to sort the results by. Defaults to signed_up_at" } }),
     desc: yupBoolean().optional().meta({ openapiField: { onlyShowInOperations: [ 'List' ], description: "Whether to sort the results in descending order. Defaults to false" } }),
-    query: yupString().optional().meta({ openapiField: { onlyShowInOperations: [ 'List' ], description: "A search query to filter the results by. This is a free-text search that is applied to the user's display name and primary email." } }),
+    query: yupString().optional().meta({ openapiField: { onlyShowInOperations: [ 'List' ], description: "A search query to filter the results by. This is a free-text search that is applied to the user's id (exact-match only), display name and primary email." } }),
   }),
   onRead: async ({ auth, params }) => {
     const user = await getUser({ projectId: auth.project.id, userId: params.user_id });
@@ -465,6 +466,9 @@ export const usersCrudHandlers = createLazyProxy(() => createCrudHandlers(usersC
       } : {},
       ...query.query ? {
         OR: [
+          ...(isUuid(query.query) ? [{
+            equals: query.query
+          }] : []),
           {
             displayName: {
               contains: query.query,
