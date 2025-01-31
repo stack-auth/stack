de33a8d5db06c37e461c94387ddda24f352476dd
CactusBlue <thecactusblue@gmail.com>
2025-01-30 16:25:29 -0800

    Very basic user id filtering (#412)

--- apps/backend/src/app/api/v1/users/crud.tsx
+++ apps/backend/src/app/api/v1/users/crud.tsx
@@ -466,6 +466,11 @@ export const usersCrudHandlers = createLazyProxy(() => createCrudHandlers(usersC
       ...query.query ? {
         OR: [
           {
+            userId: {
+              equals: query.query,
+            }
+          },
+          {
             displayName: {
               contains: query.query,
               mode: 'insensitive',
