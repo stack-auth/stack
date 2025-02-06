238dca3310a717632fe827bca3769cbf7d2c3000
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-02-05 18:20:53 -0800

    Add react-scan to dev environment

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -70,6 +70,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <head>
+        {getNodeEnvironment() === 'development' && <script src="https://unpkg.com/react-scan/dist/auto.global.js" async />}
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <StyleLink href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded&display=block" />
         <StyleLink defer href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.css" integrity="sha384-OH8qNTHoMMVNVcKdKewlipV4SErXqccxxlg6HC9Cwjr5oZu2AdBej1TndeCirael" crossOrigin="anonymous" />
