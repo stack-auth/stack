d059aa2749e06c2fd433b3322e94a3a67b93e72c
Konstantin Wohlwend <n2d4xc@gmail.com>
2025-02-06 17:47:01 -0800

    Fix migration

--- apps/backend/prisma/migrations/20250206063807_tenancies/migration.sql
+++ apps/backend/prisma/migrations/20250206063807_tenancies/migration.sql
@@ -132,18 +132,20 @@ DROP INDEX "TeamMemberDirectPermission_projectId_projectUserId_teamId_s_key";
 
 -- Create a Tenancy table
 CREATE TABLE "Tenancy" (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  createdAt TIMESTAMP NOT NULL DEFAULT now(),
-  updatedAt TIMESTAMP NOT NULL DEFAULT now(),
-  projectId TEXT NOT NULL,
-  branchId TEXT NOT NULL,
-  organizationId UUID,
-  hasNoOrganization BOOLEAN NOT NULL
+  "id" UUID NOT NULL,
+  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" TIMESTAMP(3) NOT NULL,
+  "projectId" TEXT NOT NULL,
+  "branchId" TEXT NOT NULL,
+  "organizationId" UUID,
+  "hasNoOrganization" "BooleanTrue",
+
+  CONSTRAINT "Tenancy_pkey" PRIMARY KEY ("id")
 );
 
 -- Create a Tenancy for each Project (using branch 'main' and no organization)
-INSERT INTO "Tenancy" (id, createdAt, updatedAt, projectId, branchId, organizationId, hasNoOrganization)
-SELECT uuid_generate_v4(), now(), now(), id, 'main', NULL, 'TRUE'
+INSERT INTO "Tenancy" (id, "createdAt", "updatedAt", "projectId", "branchId", "organizationId", "hasNoOrganization")
+SELECT gen_random_uuid(), now(), now(), id, 'main', NULL, 'TRUE'
 FROM "Project";
 
 /* ===== Update AuthMethod table ===== */
@@ -151,10 +153,10 @@ ALTER TABLE "AuthMethod" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "AuthMethod"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "AuthMethod"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "AuthMethod"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "AuthMethod" DROP CONSTRAINT "AuthMethod_pkey";
 ALTER TABLE "AuthMethod" DROP COLUMN "projectId";
 ALTER TABLE "AuthMethod" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "AuthMethod" DROP CONSTRAINT "AuthMethod_pkey";
 ALTER TABLE "AuthMethod" ADD CONSTRAINT "AuthMethod_pkey" PRIMARY KEY ("tenancyId", "id");
 
 /* ===== Update ConnectedAccount table ===== */
@@ -162,10 +164,10 @@ ALTER TABLE "ConnectedAccount" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ConnectedAccount"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ConnectedAccount"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ConnectedAccount"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ConnectedAccount" DROP CONSTRAINT "ConnectedAccount_pkey";
 ALTER TABLE "ConnectedAccount" DROP COLUMN "projectId";
 ALTER TABLE "ConnectedAccount" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ConnectedAccount" DROP CONSTRAINT "ConnectedAccount_pkey";
 ALTER TABLE "ConnectedAccount" ADD CONSTRAINT "ConnectedAccount_pkey" PRIMARY KEY ("tenancyId", "id");
 
 /* ===== Update ContactChannel table ===== */
@@ -173,10 +175,10 @@ ALTER TABLE "ContactChannel" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ContactChannel"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ContactChannel"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ContactChannel"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ContactChannel" DROP CONSTRAINT "ContactChannel_pkey";
 ALTER TABLE "ContactChannel" DROP COLUMN "projectId";
 ALTER TABLE "ContactChannel" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ContactChannel" DROP CONSTRAINT "ContactChannel_pkey";
 ALTER TABLE "ContactChannel" ADD CONSTRAINT "ContactChannel_pkey" PRIMARY KEY ("tenancyId", "projectUserId", "id");
 
 /* ===== Update OAuthAccessToken table ===== */
@@ -184,7 +186,7 @@ ALTER TABLE "OAuthAccessToken" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "OAuthAccessToken"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "OAuthAccessToken"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "OAuthAccessToken"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
 ALTER TABLE "OAuthAccessToken" DROP COLUMN "projectId";
 ALTER TABLE "OAuthAccessToken" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
 
@@ -193,10 +195,10 @@ ALTER TABLE "OAuthAuthMethod" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "OAuthAuthMethod"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "OAuthAuthMethod"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "OAuthAuthMethod"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "OAuthAuthMethod" DROP CONSTRAINT "OAuthAuthMethod_pkey";
 ALTER TABLE "OAuthAuthMethod" DROP COLUMN "projectId";
 ALTER TABLE "OAuthAuthMethod" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "OAuthAuthMethod" DROP CONSTRAINT "OAuthAuthMethod_pkey";
 ALTER TABLE "OAuthAuthMethod" ADD CONSTRAINT "OAuthAuthMethod_pkey" PRIMARY KEY ("tenancyId", "authMethodId");
 
 /* ===== Update OAuthToken table ===== */
@@ -204,7 +206,7 @@ ALTER TABLE "OAuthToken" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "OAuthToken"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "OAuthToken"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "OAuthToken"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
 ALTER TABLE "OAuthToken" DROP COLUMN "projectId";
 ALTER TABLE "OAuthToken" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
 
@@ -213,10 +215,10 @@ ALTER TABLE "OtpAuthMethod" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "OtpAuthMethod"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "OtpAuthMethod"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "OtpAuthMethod"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "OtpAuthMethod" DROP CONSTRAINT "OtpAuthMethod_pkey";
 ALTER TABLE "OtpAuthMethod" DROP COLUMN "projectId";
 ALTER TABLE "OtpAuthMethod" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "OtpAuthMethod" DROP CONSTRAINT "OtpAuthMethod_pkey";
 ALTER TABLE "OtpAuthMethod" ADD CONSTRAINT "OtpAuthMethod_pkey" PRIMARY KEY ("tenancyId", "authMethodId");
 
 /* ===== Update PasskeyAuthMethod table ===== */
@@ -224,10 +226,10 @@ ALTER TABLE "PasskeyAuthMethod" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "PasskeyAuthMethod"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "PasskeyAuthMethod"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "PasskeyAuthMethod"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "PasskeyAuthMethod" DROP CONSTRAINT "PasskeyAuthMethod_pkey";
 ALTER TABLE "PasskeyAuthMethod" DROP COLUMN "projectId";
 ALTER TABLE "PasskeyAuthMethod" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "PasskeyAuthMethod" DROP CONSTRAINT "PasskeyAuthMethod_pkey";
 ALTER TABLE "PasskeyAuthMethod" ADD CONSTRAINT "PasskeyAuthMethod_pkey" PRIMARY KEY ("tenancyId", "authMethodId");
 
 /* ===== Update PasswordAuthMethod table ===== */
@@ -235,10 +237,10 @@ ALTER TABLE "PasswordAuthMethod" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "PasswordAuthMethod"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "PasswordAuthMethod"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "PasswordAuthMethod"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "PasswordAuthMethod" DROP CONSTRAINT "PasswordAuthMethod_pkey";
 ALTER TABLE "PasswordAuthMethod" DROP COLUMN "projectId";
 ALTER TABLE "PasswordAuthMethod" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "PasswordAuthMethod" DROP CONSTRAINT "PasswordAuthMethod_pkey";
 ALTER TABLE "PasswordAuthMethod" ADD CONSTRAINT "PasswordAuthMethod_pkey" PRIMARY KEY ("tenancyId", "authMethodId");
 
 /* ===== Update Permission table ===== */
@@ -246,7 +248,7 @@ ALTER TABLE "Permission" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "Permission"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "Permission"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "Permission"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
 ALTER TABLE "Permission" DROP COLUMN "projectId";
 ALTER TABLE "Permission" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
 
@@ -255,10 +257,10 @@ ALTER TABLE "ProjectUser" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ProjectUser"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ProjectUser"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ProjectUser"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ProjectUser" DROP CONSTRAINT "ProjectUser_pkey";
 ALTER TABLE "ProjectUser" DROP COLUMN "projectId";
 ALTER TABLE "ProjectUser" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ProjectUser" DROP CONSTRAINT "ProjectUser_pkey";
 ALTER TABLE "ProjectUser" ADD CONSTRAINT "ProjectUser_pkey" PRIMARY KEY ("tenancyId", "projectUserId");
 
 /* ===== Update ProjectUserAuthorizationCode table ===== */
@@ -266,10 +268,10 @@ ALTER TABLE "ProjectUserAuthorizationCode" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ProjectUserAuthorizationCode"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ProjectUserAuthorizationCode"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ProjectUserAuthorizationCode"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ProjectUserAuthorizationCode" DROP CONSTRAINT "ProjectUserAuthorizationCode_pkey";
 ALTER TABLE "ProjectUserAuthorizationCode" DROP COLUMN "projectId";
 ALTER TABLE "ProjectUserAuthorizationCode" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ProjectUserAuthorizationCode" DROP CONSTRAINT "ProjectUserAuthorizationCode_pkey";
 ALTER TABLE "ProjectUserAuthorizationCode" ADD CONSTRAINT "ProjectUserAuthorizationCode_pkey" PRIMARY KEY ("tenancyId", "authorizationCode");
 
 /* ===== Update ProjectUserOAuthAccount table ===== */
@@ -277,10 +279,10 @@ ALTER TABLE "ProjectUserOAuthAccount" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ProjectUserOAuthAccount"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ProjectUserOAuthAccount"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ProjectUserOAuthAccount"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ProjectUserOAuthAccount" DROP CONSTRAINT "ProjectUserOAuthAccount_pkey";
 ALTER TABLE "ProjectUserOAuthAccount" DROP COLUMN "projectId";
 ALTER TABLE "ProjectUserOAuthAccount" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ProjectUserOAuthAccount" DROP CONSTRAINT "ProjectUserOAuthAccount_pkey";
 ALTER TABLE "ProjectUserOAuthAccount" ADD CONSTRAINT "ProjectUserOAuthAccount_pkey" PRIMARY KEY ("tenancyId", "oauthProviderConfigId", "providerAccountId");
 
 /* ===== Update ProjectUserRefreshToken table ===== */
@@ -288,10 +290,10 @@ ALTER TABLE "ProjectUserRefreshToken" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "ProjectUserRefreshToken"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "ProjectUserRefreshToken"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "ProjectUserRefreshToken"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "ProjectUserRefreshToken" DROP CONSTRAINT "ProjectUserRefreshToken_pkey";
 ALTER TABLE "ProjectUserRefreshToken" DROP COLUMN "projectId";
 ALTER TABLE "ProjectUserRefreshToken" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "ProjectUserRefreshToken" DROP CONSTRAINT "ProjectUserRefreshToken_pkey";
 ALTER TABLE "ProjectUserRefreshToken" ADD CONSTRAINT "ProjectUserRefreshToken_pkey" PRIMARY KEY ("tenancyId", "refreshToken");
 
 /* ===== Update Team table ===== */
@@ -299,10 +301,10 @@ ALTER TABLE "Team" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "Team"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "Team"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "Team"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "Team" DROP CONSTRAINT "Team_pkey";
 ALTER TABLE "Team" DROP COLUMN "projectId";
 ALTER TABLE "Team" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "Team" DROP CONSTRAINT "Team_pkey";
 ALTER TABLE "Team" ADD CONSTRAINT "Team_pkey" PRIMARY KEY ("tenancyId", "teamId");
 
 /* ===== Update TeamMember table ===== */
@@ -310,10 +312,10 @@ ALTER TABLE "TeamMember" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "TeamMember"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "TeamMember"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "TeamMember"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
+ALTER TABLE "TeamMember" DROP CONSTRAINT "TeamMember_pkey";
 ALTER TABLE "TeamMember" DROP COLUMN "projectId";
 ALTER TABLE "TeamMember" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
-ALTER TABLE "TeamMember" DROP CONSTRAINT "TeamMember_pkey";
 ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_pkey" PRIMARY KEY ("tenancyId", "projectUserId", "teamId");
 
 /* ===== Update TeamMemberDirectPermission table ===== */
@@ -321,7 +323,7 @@ ALTER TABLE "TeamMemberDirectPermission" ADD COLUMN "tenancyId_temp" UUID;
 UPDATE "TeamMemberDirectPermission"
 SET "tenancyId_temp" = t.id
 FROM "Tenancy" t
-WHERE t.projectId = "TeamMemberDirectPermission"."projectId" AND t.branchId = 'main' AND t.organizationId IS NULL;
+WHERE t."projectId" = "TeamMemberDirectPermission"."projectId" AND t."branchId" = 'main' AND t."organizationId" IS NULL;
 ALTER TABLE "TeamMemberDirectPermission" DROP COLUMN "projectId";
 ALTER TABLE "TeamMemberDirectPermission" RENAME COLUMN "tenancyId_temp" TO "tenancyId";
 
