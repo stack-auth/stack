93e5c06e2acbe3fe4837613ba39a84cc534f427d
Zai Shi <zaishi00@outlook.com>
2025-02-11 11:32:25 -0800

    fixed theme toggle

--- apps/dashboard/src/components/theme-toggle.tsx
+++ apps/dashboard/src/components/theme-toggle.tsx
@@ -3,13 +3,13 @@ import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export default function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   return (
     <Button
       variant="outline"
       size="icon"
       className="w-8 h-8"
-      onClick={() => setTheme(theme => theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <SunIcon className="hidden dark:block w-4 h-4" />
       <MoonIcon className="block dark:hidden w-4 h-4" />
