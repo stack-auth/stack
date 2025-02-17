706d55fdea91486e69af3c64cf1b20a20c8faf52
Zai Shi <zaishi00@outlook.com>
2025-02-16 18:39:02 -0800

    added config schema

--- packages/stack-shared/src/config/parser.ts
+++ packages/stack-shared/src/config/parser.ts
@@ -1,10 +1,82 @@
 // Check out https://github.com/stack-auth/info/blob/main/eng-handbook/random-thoughts/config-json-format.md for more information on the config format
 
 import * as yup from "yup";
-import { yupBoolean, yupNumber, yupObject, yupRecord, yupString, yupTuple } from "../schema-fields";
+import * as schemaFields from "../schema-fields";
+import { yupArray, yupBoolean, yupNumber, yupObject, yupRecord, yupString, yupTuple } from "../schema-fields";
 import { deepPlainClone } from "../utils/objects";
 
+type EnvMode = 'no-env' | 'optional-env' | 'required-env';
+
 const configRecord = (schema: yup.AnySchema) => yupRecord(schema, (key) => key.match(/^[a-zA-Z0-9_]+$/) !== null);
+
+const envSchema = <T extends yup.AnySchema>(mode: EnvMode, schema: T): T => {
+  switch (mode) {
+    case 'no-env': {
+      return schema;
+    }
+    case 'optional-env': {
+      return schema.nullable().optional();
+    }
+    case 'required-env': {
+      return schema;
+    }
+  }
+};
+
+export const getConfigSchema = (mode: EnvMode) => yupObject({
+  isProductionMode: yupBoolean().defined(),
+  allowLocalhost: yupBoolean().defined(),
+  signUpEnabled: yupBoolean().defined(),
+  createTeamOnSignUp: yupBoolean().defined(),
+  clientTeamCreationEnabled: yupBoolean().defined(),
+  clientUserDeletionEnabled: yupBoolean().defined(),
+  legacyGlobalJwtSigning: yupBoolean().defined(),
+
+  // keys to the permissions/permission definitions are hex encoded ids.
+  teamCreateDefaultSystemPermissions: configRecord(yupObject({
+    id: yupString().defined(),
+  })).defined(),
+  teamMemberDefaultSystemPermissions: configRecord(yupObject({
+    id: yupString().defined(),
+  })).defined(),
+  permissionDefinitions: configRecord(yupObject({
+    id: yupString().defined(),
+    description: yupString().defined(),
+    containedPermissionIds: yupArray(yupString()).defined(),
+  })).defined(),
+
+  // keys to the oauth provider configs are the provider ids.
+  oauthProviderConfigs: configRecord(yupObject({
+    id: yupString().defined(),
+    type: envSchema(mode, yupString().oneOf(['shared', 'standard']).defined()),
+    clientId: envSchema(mode, schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.oauthClientIdSchema, { type: 'standard', enabled: true })),
+    clientSecret: envSchema(mode, schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.oauthClientSecretSchema, { type: 'standard', enabled: true })),
+    facebookConfigId: envSchema(mode, schemaFields.oauthFacebookConfigIdSchema.optional()),
+    microsoftTenantId: envSchema(mode, schemaFields.oauthMicrosoftTenantIdSchema.optional()),
+  })).defined(),
+
+  emailConfig: envSchema(mode, yupObject({
+    type: schemaFields.emailTypeSchema.defined(),
+    host: schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.emailHostSchema, {
+      type: 'standard',
+    }),
+    port: schemaFields.yupDefinedWhen(schemaFields.emailPortSchema, {
+      type: 'standard',
+    }),
+    username: schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.emailUsernameSchema, {
+      type: 'standard',
+    }),
+    password: schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.emailPasswordSchema, {
+      type: 'standard',
+    }),
+    sender_name: schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.emailSenderNameSchema, {
+      type: 'standard',
+    }),
+    sender_email: schemaFields.yupDefinedAndNonEmptyWhen(schemaFields.emailSenderEmailSchema, {
+      type: 'standard',
+    }),
+  })),
+});
 
 export function mergeConfigs(options: {
   configs: any[],
